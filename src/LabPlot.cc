bcee638b42050c7644ca4674822676f3627954cb
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2008-10-06 08:44:56 +0000

    added source header with copyright

--- src/LabPlot.cc
+++ src/LabPlot.cc
@@ -1,3 +1,31 @@
+/***************************************************************************
+    File                 : LabPlot.cc
+    Project              : LabPlot
+    --------------------------------------------------------------------
+    Copyright            : (C) 2008 by Stefan Gerlach
+    Email (use @ for *)  : stefan.gerlach*uni-konstanz.de
+    Description          : main class
+                           
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor,                    *
+ *   Boston, MA  02110-1301  USA                                           *
+ *                                                                         *
+ ***************************************************************************/
 #include <KApplication>
 #include <KAboutData>
 #include <KCmdLineArgs>
@@ -14,7 +42,7 @@ int main (int argc, char *argv[]) {
 			KAboutData::License_GPL,
 			ki18n("Copyright (c) 2008 Stefan Gerlach") );
 	aboutData.addAuthor(ki18n("Stefan Gerlach"), ki18n("developer"), "stefan.gerlach@uni-konstanz.de", 0);
-	aboutData.addAuthor(ki18n("Alexander Semke"), ki18n("developer"), "", 0);
+	aboutData.addAuthor(ki18n("Alexander Semke"), ki18n("developer"), "alexander.semke@web.de", 0);
 
 	KCmdLineArgs::init( argc, argv, &aboutData );
 	KCmdLineOptions options;
--- src/TableModel.h
+++ src/TableModel.h
@@ -1,4 +1,31 @@
-//LabPlot: TableModel.h
+/***************************************************************************
+    File                 : TableModel.h
+    Project              : LabPlot
+    --------------------------------------------------------------------
+    Copyright            : (C) 2008 by Stefan Gerlach
+    Email (use @ for *)  : stefan.gerlach*uni-konstanz.de
+    Description          : model for table data
+                           
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor,                    *
+ *   Boston, MA  02110-1301  USA                                           *
+ *                                                                         *
+ ***************************************************************************/
 
 #ifndef TABLEMODEL_H
 #define TABLEMODEL_H
