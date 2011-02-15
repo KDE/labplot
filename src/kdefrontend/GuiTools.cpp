72742ad9f7269a275116451cdbcb65c8343b8e12
Alexander Semke <alexander.semke@web.de>
2011-02-15 22:26:18 +0000

    Added GuiTools. This class is supposed to contain several static functions which are used frequently throughout the kde frontend.

---
+++ src/kdefrontend/GuiTools.cpp
@@ -0,0 +1,96 @@
+/***************************************************************************
+    File                 : GuiTools.cpp
+    Project              : LabPlot
+    --------------------------------------------------------------------
+    Copyright            : (C) 2011 Alexander Semke (alexander.semke*web.de)
+                           (replace * with @ in the email addresses)
+    Description          :  constains several static functions which are used on frequently throughout the kde frontend.
+                           
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor,                    *
+ *   Boston, MA  02110-1301  USA                                           *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#include "GuiTools.h"
+#include <KLocale>
+
+/*!
+	fills the ComboBox \c combobox with the six possible Qt::PenStyles, the color \c color is used.
+*/
+void GuiTools::updatePenStyles(QComboBox* comboBox, const QColor& color){
+	int index=comboBox->currentIndex();
+	comboBox->clear();
+
+	QPainter pa;
+	int offset=2;
+	int w=50;
+	int h=10;
+	QPixmap pm( w, h );
+	comboBox->setIconSize( QSize(w,h) );
+	
+	//loop over six possible Qt-PenStyles, draw on the pixmap and insert it
+	QStringList list=QStringList()<<i18n("no line")<<i18n("solid line")<<i18n("dash line")<<i18n("dot line")
+														<<i18n("dash-dot line")<<i18n("dash-dot-dot line");
+	for (int i=0;i<6;i++){
+		pm.fill(Qt::transparent);
+		pa.begin( &pm );
+		pa.setPen( QPen( color, 1, (Qt::PenStyle)i ) );
+		pa.drawLine( offset, h/2, w-offset, h/2);
+		pa.end();
+ 		comboBox->addItem( QIcon(pm), list.at(i) );
+	}
+	comboBox->setCurrentIndex(index);
+}
+
+
+/*!
+	fills the ComboBox for the symbol filling patterns with the 14 possible Qt::BrushStyles.
+*/
+void GuiTools::updateBrushStyles(QComboBox* comboBox, const QColor& color){
+  	int index=comboBox->currentIndex();
+	comboBox->clear();
+
+	QPainter pa;
+	int offset=2;
+	int w=50;
+	int h=20;
+	QPixmap pm( w, h );
+	comboBox->setIconSize( QSize(w,h) );
+
+	QPen pen(Qt::SolidPattern, 1);
+ 	pa.setPen( pen );
+	
+	QStringList list=QStringList()<<i18n("none")<<i18n("uniform")<<i18n("extremely dense")<<i18n("very dense")
+														<<i18n("somewhat dense")<<i18n("half dense")<<i18n("somewhat sparce")
+														 <<i18n("very sparce")<<i18n("extremely sparce")<<i18n("horiz. lines")
+														 <<i18n("vert. lines")<<i18n("crossing lines")<<i18n("backward diag. lines")
+														 <<i18n("forward diag. lines")<<i18n("crossing diag. lines");
+	for (int i=0;i<15;i++) {
+		pm.fill(Qt::transparent);
+		pa.begin( &pm );
+		pa.setRenderHint(QPainter::Antialiasing);
+ 		pa.setBrush( QBrush(color, (Qt::BrushStyle)i) );
+		pa.drawRect( offset, offset, w - 2*offset, h - 2*offset);
+		pa.end();
+		comboBox->addItem( QIcon(pm), list.at(i) );
+	}
+
+	comboBox->setCurrentIndex(index);
+}
