16592a268cf32cf01b4e6800f21f9c3d4213e54d
Minh Ngo <nlminhtl@gmail.com>
2015-07-29 11:49:26 +0200

    Adding license to unit tests

--- src/tests/DockWidgets/Surface3DDockTest.cpp
+++ src/tests/DockWidgets/Surface3DDockTest.cpp
@@ -1,3 +1,30 @@
+/***************************************************************************
+    File                 : Surface3DDockTest.cpp
+    Project              : LabPlot
+    Description          : Surface3DDock tests
+    --------------------------------------------------------------------
+    Copyright            : (C) 2015 Minh Ngo (minh@fedoraproject.org)
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor,                    *
+ *   Boston, MA  02110-1301  USA                                           *
+ *                                                                         *
+ ***************************************************************************/
 #include "Surface3DDockTest.h"
 #include "backend/worksheet/plots/3d/Surface3D.h"
 #include "kdefrontend/dockwidgets/Surface3DDock.h"
