dbdd2cfd58486e1ada20886b57ec032ec1353850
Anton Kreuzkamp <kde-development@akreuzkamp.de>
2015-12-03 13:28:44 +0100

    Resize fitData.paramStartValues on init if needed.
    When initializing fitData.paramNames is filled with data, according to
    the model type. But paramStartValues isn't because it's expected to be
    filled with data from the xml-file. But it only is, if there is data
    in the xml-file. But when newly creating it, there's no such data.
    Thus the sizes of the two lists would get out of sync and eventually
    crash.

--- src/kdefrontend/dockwidgets/XYFitCurveDock.cpp
+++ src/kdefrontend/dockwidgets/XYFitCurveDock.cpp
@@ -430,8 +430,10 @@ void XYFitCurveDock::updateModelEquation() {
 
 	//resize the vector for the start values and set the elements to 1.0
 	//in case a custom model is used, do nothing, we take over the previous values
-	//when initalizing, don't do anything - we use start values already available
-	if (m_fitData.modelType!=XYFitCurve::Custom && !m_initializing) {
+	//when initalizing, don't do anything - we use start values already
+	//available - unless there's no values available
+	if (m_fitData.modelType!=XYFitCurve::Custom &&
+            !(m_initializing && m_fitData.paramNames.size() == m_fitData.paramStartValues.size())) {
 		m_fitData.paramStartValues.resize(m_fitData.paramNames.size());
 		for (int i=0; i<m_fitData.paramStartValues.size(); ++i)
 			m_fitData.paramStartValues[i] = 1.0;
