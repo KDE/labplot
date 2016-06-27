adc06d16f29ce09ae9f4706fed1ef120defdf057
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2016-06-27 20:07:16 +0200

    added window function files to nsl

---
+++ src/backend/nsl/nsl_sf_window.h
@@ -0,0 +1,39 @@
+/***************************************************************************
+    File                 : nsl_sf_window.h
+    Project              : LabPlot
+    Description          : NSL special window functions
+    --------------------------------------------------------------------
+    Copyright            : (C) 2016 by Stefan Gerlach (stefan.gerlach@uni.kn)
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor,                    *
+ *   Boston, MA  02110-1301  USA                                           *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef NSL_SF_WINDOW_H
+#define NSL_SF_WINDOW_H
+
+#include <stdlib.h>
+
+/* u range: [0:1] or [0:N-1] ? */
+
+/* uniform */
+double nsl_sf_window_uniform(double u);
+
+#endif /* NSL_SF_WINDOW_H */
