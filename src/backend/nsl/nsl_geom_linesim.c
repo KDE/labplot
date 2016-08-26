8df2288fb182e9e3e80b5fd638aa56a5288d4cb9
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2016-08-26 23:53:47 +0200

    added NSL files for line simplification

---
+++ src/backend/nsl/nsl_geom_linesim.c
@@ -0,0 +1,35 @@
+/***************************************************************************
+    File                 : nsl_geom_linesim.c
+    Project              : LabPlot
+    Description          : NSL geometry line simplification functions
+    --------------------------------------------------------------------
+    Copyright            : (C) 2016 by Stefan Gerlach (stefan.gerlach@uni.kn)
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor,                    *
+ *   Boston, MA  02110-1301  USA                                           *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <math.h>
+#include "nsl_geom_linesim.h"
+
+
+double nsl_geom_linesim_nthpoint(const double data[], size_t n) {
+	/*TODO*/
+}
