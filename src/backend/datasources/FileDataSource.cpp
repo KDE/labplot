f73de389e6d13c74ad825d19397a9adeb38a3ade
Alexander Semke <alexander.semke@web.de>
2017-03-30 21:26:40 +0200

    Minor i18n fixes.

--- src/backend/datasources/FileDataSource.cpp
+++ src/backend/datasources/FileDataSource.cpp
@@ -257,7 +257,7 @@ QString FileDataSource::fileInfoString(const QString &name){
 		QStringList infoStrings;
 
 		//general information about the file
-		infoStrings << i18n("<u><b>%1:</b></u>", fileName);
+		infoStrings << "<u><b>" + fileName + "</b></u><br>";
 		fileInfo.setFile(fileName);
 
 		infoStrings << i18n("Readable: %1", fileInfo.isReadable() ? i18n("yes") : i18n("no"));
