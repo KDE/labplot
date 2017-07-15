d826507ddf509fc458230fd2e4b62040128603ad
Alexander Semke <alexander.semke@web.de>
2017-07-15 20:01:41 +0200

    Added checkboxes in the preview table in ImportSQLDatabaseWidget to determine which columns to import.

--- src/kdefrontend/datasources/ImportSQLDatabaseWidget.cpp
+++ src/kdefrontend/datasources/ImportSQLDatabaseWidget.cpp
@@ -242,6 +242,7 @@ void ImportSQLDatabaseWidget::refreshPreview() {
 	QLocale::Language numberFormat = (QLocale::Language)ui.cbNumberFormat->currentIndex();
 	const QString& dateTimeFormat = ui.cbDateTimeFormat->currentText();
 	q.next(); //go to the first record
+	ui.twPreview->setRowCount(1); //add the first row for the check boxes
 	for (int i = 0; i < m_cols; ++i) {
 		//name
 		m_columnNames << q.record().fieldName(i);
@@ -258,11 +259,16 @@ void ImportSQLDatabaseWidget::refreshPreview() {
 		item->setTextAlignment(Qt::AlignLeft);
 		item->setIcon(AbstractColumn::iconForMode(mode));
 		ui.twPreview->setHorizontalHeaderItem(i, item);
+
+		//create checked items
+		QTableWidgetItem* itemChecked = new QTableWidgetItem();
+		itemChecked->setCheckState(Qt::Checked);
+		ui.twPreview->setItem(0, i, itemChecked);
 	}
 
 	//preview the data
 	const bool customQuery = (ui.cbImportFrom->currentIndex() != 0);
-	int row = 0;
+	int row = 1;
 	do {
 		for(int col = 0; col < m_cols; ++col) {
 			ui.twPreview->setRowCount(row+1);
