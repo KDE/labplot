4551f181adcfc900cba8744b4c77ad1021dda0fe
Fabian Kristof <f-kristof@hotmail.com>
2017-07-18 15:56:37 +0200

    Build fixed.

--- src/kdefrontend/dockwidgets/LiveDataDock.cpp
+++ src/kdefrontend/dockwidgets/LiveDataDock.cpp
@@ -32,7 +32,7 @@ LiveDataDock::LiveDataDock(QWidget *parent) :
 
 	connect(ui.bPausePlayReading, SIGNAL(clicked(bool)), this, SLOT(pauseContinueReading()));
 	connect(ui.bUpdateNow, SIGNAL(clicked(bool)), this, SLOT(updateNow()));
-	connect(ui.sbUpdateFrequency, SIGNAL(valueChanged(int)), this, SLOT(updateIntervalChanged(int)));
+    connect(ui.sbUpdateInterval, SIGNAL(valueChanged(int)), this, SLOT(updateIntervalChanged(int)));
 	connect(ui.leKeepNValues, SIGNAL(textChanged(QString)), this, SLOT(keepNvaluesChanged(QString)));
 	connect(ui.sbSampleRate, SIGNAL(valueChanged(int)), this, SLOT(sampleRateChanged(int)));
 	connect(ui.cbUpdateType, SIGNAL(currentIndexChanged(int)), this, SLOT(updateTypeChanged(int)));
@@ -48,7 +48,7 @@ LiveDataDock::~LiveDataDock() {
 void LiveDataDock::setLiveDataSources(const QList<FileDataSource *> &sources) {
 	m_liveDataSources = sources;
 	const FileDataSource* const fds = sources.at(0);
-	ui.sbUpdateFrequency->setValue(fds->updateInterval());
+    ui.sbUpdateInterval->setValue(fds->updateInterval());
 	ui.cbUpdateType->setCurrentIndex(static_cast<int>(fds->updateType()));
 
 	if (fds->updateType() == FileDataSource::UpdateType::NewData) {
@@ -96,17 +96,17 @@ void LiveDataDock::updateTypeChanged(int idx) {
 	FileDataSource::UpdateType type = static_cast<FileDataSource::UpdateType>(idx);
 
 	if (type == FileDataSource::UpdateType::TimeInterval) {
-		ui.lUpdateFrequency->show();
-		ui.sbUpdateFrequency->show();
+        ui.lUpdateInterval->show();
+        ui.sbUpdateInterval->show();
 
 		for (auto* source: m_liveDataSources) {
 			source->setUpdateType(type);
-			source->setUpdateInterval(ui.sbUpdateFrequency->value());
+            source->setUpdateInterval(ui.sbUpdateInterval->value());
 		}
 
 	} else if (type == FileDataSource::UpdateType::NewData) {
-		ui.lUpdateFrequency->hide();
-		ui.sbUpdateFrequency->hide();
+        ui.lUpdateInterval->hide();
+        ui.sbUpdateInterval->hide();
 
 		for (auto* source: m_liveDataSources)
 			source->setUpdateType(type);
