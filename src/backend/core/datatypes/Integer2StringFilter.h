e938a4d026a6d0012fb11a0a662b8b0ad565e342
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2017-07-20 00:32:39 +0200

    adding new integer filter

---
+++ src/backend/core/datatypes/Integer2StringFilter.h
@@ -0,0 +1,64 @@
+/***************************************************************************
+    File                 : Integer2StringFilter.h
+    Project              : AbstractColumn
+    --------------------------------------------------------------------
+    Copyright            : (C) 2017 Stefan Gerlach (stefan.gerlach@uni.kn)
+    Description          : Locale-aware conversion filter int -> QString.
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor,                    *
+ *   Boston, MA  02110-1301  USA                                           *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef INTEGER2STRING_FILTER_H
+#define INTEGER2STRING_FILTER_H
+
+#include "../AbstractSimpleFilter.h"
+#include <QLocale>
+#include <cmath>
+
+//! Locale-aware conversion filter int -> QString.
+class Integer2StringFilter : public AbstractSimpleFilter {
+	Q_OBJECT
+
+public:
+	//! Standard constructor.
+	explicit Integer2StringFilter() {}
+
+	//! Return the data type of the column
+	virtual AbstractColumn::ColumnMode columnMode() const { return AbstractColumn::Text; }
+
+public:
+	virtual QString textAt(int row) const {
+		if (!m_inputs.value(0)) return QString();
+		if (m_inputs.value(0)->rowCount() <= row) return QString();
+
+		int inputValue = m_inputs.value(0)->integerAt(row);
+		if (std::isnan(inputValue)) return QString();
+
+		return QLocale().toString(inputValue);
+	}
+
+protected:
+	//! Using typed ports: only double inputs are accepted.
+	virtual bool inputAcceptable(int, const AbstractColumn *source) {
+		return source->columnMode() == AbstractColumn::Integer;
+	}
+};
+
+#endif // ifndef INTEGER2STRING_FILTER_H
+
---
+++ src/backend/core/datatypes/String2IntegerFilter.h
@@ -0,0 +1,76 @@
+/***************************************************************************
+    File                 : String2IntegerFilter.h
+    Project              : AbstractColumn
+    --------------------------------------------------------------------
+    Copyright            : (C) 2017 Stefan Gerlach (stefan.gerlach@uni.kn)
+    Description          : Locale-aware conversion filter QString -> int.
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor,                    *
+ *   Boston, MA  02110-1301  USA                                           *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef STRING2INTEGER_FILTER_H
+#define STRING2INTEGER_FILTER_H
+
+#include "../AbstractSimpleFilter.h"
+#include <QLocale>
+
+//! Locale-aware conversion filter QString -> int.
+class String2IntegerFilter : public AbstractSimpleFilter {
+	Q_OBJECT
+
+public:
+	String2IntegerFilter() : m_use_default_locale(true) {}
+	void setNumericLocale(QLocale locale) { m_numeric_locale = locale; m_use_default_locale = false; }
+	void setNumericLocaleToDefault() { m_use_default_locale = true; }
+
+	virtual int integerAt(int row) const {
+		DEBUG("String2Integer::integerAt()");
+		if (!m_inputs.value(0)) return 0;
+
+		int result;
+		bool valid;
+		QString textValue = m_inputs.value(0)->textAt(row);
+		DEBUG("	textValue = " << textValue.toStdString());
+		if (m_use_default_locale) // we need a new QLocale instance here in case the default changed since the last call
+			result = QLocale().toInt(textValue, &valid);
+		else
+			result = m_numeric_locale.toInt(textValue, &valid);
+		DEBUG("	result = " << result << " valid = " << valid);
+
+		if (valid)
+			return result;
+		return NAN;
+	}
+
+	//! Return the data type of the column
+	virtual AbstractColumn::ColumnMode columnMode() const { return AbstractColumn::Integer; }
+
+protected:
+	//! Using typed ports: only string inputs are accepted.
+	virtual bool inputAcceptable(int, const AbstractColumn *source) {
+		return source->columnMode() == AbstractColumn::Text;
+	}
+
+private:
+	QLocale m_numeric_locale;
+	bool m_use_default_locale;
+};
+
+#endif // ifndef STRING2INTEGER_FILTER_H
