aa52a63f76be86dd98bbdff20465a3e118850546
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2017-07-21 01:23:29 +0200

    fixed column mode conversion

--- src/backend/core/AbstractFilter.h
+++ src/backend/core/AbstractFilter.h
@@ -34,68 +34,66 @@
 
 class AbstractColumn;
 
-class AbstractFilter : public AbstractAspect
-{
+class AbstractFilter : public AbstractAspect {
 	Q_OBJECT
 
-	public:
-		explicit AbstractFilter(const QString& name) : AbstractAspect(name) {}
-		virtual ~AbstractFilter() {}
+public:
+	explicit AbstractFilter(const QString& name) : AbstractAspect(name) {}
+	virtual ~AbstractFilter() {}
 
-		virtual int inputCount() const = 0;
-		virtual int outputCount() const = 0;
-		int highestConnectedInput() const;
-		bool input(int port, const AbstractColumn* source);
-		bool input(const AbstractFilter* sources);
-		const AbstractColumn *input(int port) const;
-		virtual QString inputLabel(int port) const;
-		virtual AbstractColumn *output(int port=0) = 0;
-		virtual const AbstractColumn *output(int port=0) const = 0;
-		
-		int portIndexOf(const AbstractColumn * column);
+	virtual int inputCount() const = 0;
+	virtual int outputCount() const = 0;
+	int highestConnectedInput() const;
+	bool input(int port, const AbstractColumn* source);
+	bool input(const AbstractFilter* sources);
+	const AbstractColumn *input(int port) const;
+	virtual QString inputLabel(int port) const;
+	virtual AbstractColumn *output(int port = 0) = 0;
+	virtual const AbstractColumn *output(int port = 0) const = 0;
 
-	protected:
-		virtual bool inputAcceptable(int port, const AbstractColumn *source);
-		virtual void inputAboutToBeDisconnected(const AbstractColumn * source);
+	int portIndexOf(const AbstractColumn* column);
 
-	protected slots:
+protected:
+	virtual bool inputAcceptable(int port, const AbstractColumn *source);
+	virtual void inputAboutToBeDisconnected(const AbstractColumn * source);
 
-		virtual void inputDescriptionAboutToChange(const AbstractColumn * source);
-		void inputDescriptionAboutToChange(const AbstractAspect * aspect);
-		virtual void inputDescriptionChanged(const AbstractColumn * source);
-		void inputDescriptionChanged(const AbstractAspect * aspect);
-		virtual void inputPlotDesignationAboutToChange(const AbstractColumn * source);
+protected slots:
+	virtual void inputDescriptionAboutToChange(const AbstractColumn * source);
+	void inputDescriptionAboutToChange(const AbstractAspect * aspect);
+	virtual void inputDescriptionChanged(const AbstractColumn * source);
+	void inputDescriptionChanged(const AbstractAspect * aspect);
+	virtual void inputPlotDesignationAboutToChange(const AbstractColumn * source);
 
-		virtual void inputPlotDesignationChanged(const AbstractColumn * source);
-		virtual void inputModeAboutToChange(const AbstractColumn * source);
-		virtual void inputModeChanged(const AbstractColumn * source);
-		virtual void inputDataAboutToChange(const AbstractColumn * source);
-		virtual void inputDataChanged(const AbstractColumn * source);
+	virtual void inputPlotDesignationChanged(const AbstractColumn * source);
+	virtual void inputModeAboutToChange(const AbstractColumn * source);
+	virtual void inputModeChanged(const AbstractColumn * source);
+	virtual void inputDataAboutToChange(const AbstractColumn * source);
+	virtual void inputDataChanged(const AbstractColumn * source);
 
-		virtual void inputRowsAboutToBeInserted(const AbstractColumn * source, int before, int count) {
-			Q_UNUSED(source); Q_UNUSED(before); Q_UNUSED(count);
-		}
-		virtual void inputRowsInserted(const AbstractColumn * source, int before, int count) {
-			Q_UNUSED(source); Q_UNUSED(before); Q_UNUSED(count);
-		}
-		virtual void inputRowsAboutToBeRemoved(const AbstractColumn * source, int first, int count) {
-			Q_UNUSED(source); Q_UNUSED(first); Q_UNUSED(count);
-		}
-		virtual void inputRowsRemoved(const AbstractColumn * source, int first, int count) {
-			Q_UNUSED(source); Q_UNUSED(first); Q_UNUSED(count);
-		}
-		virtual void inputMaskingAboutToChange(const AbstractColumn * source) {
-			Q_UNUSED(source);
-		}
-		virtual void inputMaskingChanged(const AbstractColumn * source) {
-			Q_UNUSED(source);
-		}
-		void inputAboutToBeDestroyed(const AbstractColumn * source) {
-			input(portIndexOf(source), 0);
-		}
+	virtual void inputRowsAboutToBeInserted(const AbstractColumn * source, int before, int count) {
+		Q_UNUSED(source); Q_UNUSED(before); Q_UNUSED(count);
+	}
+	virtual void inputRowsInserted(const AbstractColumn * source, int before, int count) {
+		Q_UNUSED(source); Q_UNUSED(before); Q_UNUSED(count);
+	}
+	virtual void inputRowsAboutToBeRemoved(const AbstractColumn * source, int first, int count) {
+		Q_UNUSED(source); Q_UNUSED(first); Q_UNUSED(count);
+	}
+	virtual void inputRowsRemoved(const AbstractColumn * source, int first, int count) {
+		Q_UNUSED(source); Q_UNUSED(first); Q_UNUSED(count);
+	}
+	virtual void inputMaskingAboutToChange(const AbstractColumn * source) {
+		Q_UNUSED(source);
+	}
+	virtual void inputMaskingChanged(const AbstractColumn * source) {
+		Q_UNUSED(source);
+	}
+	void inputAboutToBeDestroyed(const AbstractColumn * source) {
+		input(portIndexOf(source), 0);
+	}
 
-	protected:
-		QVector<const AbstractColumn*> m_inputs;
+protected:
+	QVector<const AbstractColumn*> m_inputs;
 };
 
 #endif // ifndef ABSTRACT_FILTER_H
