c2b0b791b473bc6288f82fe8378a2f34b957afbe
Fabian Kristof <f-kristof@hotmail.com>
2017-08-22 00:03:36 +0200

    Added cpu usage scripts for live data testing.

---
+++ tests/import_export/ASCII/scripts/server_cpu_local_socket.py
@@ -0,0 +1,21 @@
+import socket
+import psutil
+
+ADDR = './local_socket'
+serv = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+BUFSIZE = 4096
+
+serv.bind(ADDR)
+serv.listen(1)
+
+print 'listening ...'
+
+while True:
+  conn, addr = serv.accept()
+  print 'client connected ... ', addr
+  cpu_percent = str(psutil.cpu_percent())
+  conn.send(cpu_percent)
+  print 'written ' + cpu_percent
+  conn.close()
+  print 'client disconnected'
