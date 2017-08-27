94e27de54aa934f87741c37cc613fd1c5ae32db1
Alexander Semke <alexander.semke@web.de>
2017-08-27 18:45:44 +0200

    Added shebang to the test python scripts.

--- tests/import_export/ASCII/scripts/cpu_usage_file.py
+++ tests/import_export/ASCII/scripts/cpu_usage_file.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import socket
 import psutil
 import time
@@ -7,7 +9,7 @@ while True:
     cpu_percents = str(psutil.cpu_percent(interval=0.5, percpu=True))
     line = str(cpu_percents)[1:-1]
     print line
-    
+
     datafile = open("cpuData.txt","a")
     datafile.write(line + "\n")
     datafile.close()
