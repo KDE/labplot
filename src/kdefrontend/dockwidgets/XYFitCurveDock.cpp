d09d3994078aca3a7d2cd2b0124aa2b192a98c01
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2017-11-15 01:10:53 +0100

    'recalculate' in fit category changes leads to crashes -> TODO

--- src/kdefrontend/dockwidgets/XYFitCurveDock.cpp
+++ src/kdefrontend/dockwidgets/XYFitCurveDock.cpp
@@ -522,7 +522,7 @@ void XYFitCurveDock::categoryChanged(int index) {
 	modelTypeChanged(m_fitData.modelType);
 
 	m_initializing = false;
-	enableRecalculate();
+	//TODO: enableRecalculate();
 }
 
 /*!
