6fada9ca371745a77ff4ae5832068080042272ce
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2017-12-20 19:19:01 +0100

    debugfull -> debug

--- compile_windows_debug.ps1
+++ compile_windows_debug.ps1
@@ -1,6 +1,6 @@
 mkdir build
 cd build
 
-cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=DebugFull -DCMAKE_INSTALL_PREFIX=C:\KDE  ..
+cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=C:\KDE  ..
 
 mingw32-make -j 4 install
