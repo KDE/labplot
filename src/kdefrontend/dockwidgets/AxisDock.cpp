b2abfb494480fe6b3013c3d21b796b4c9fef67ed
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2018-01-20 12:08:26 +0100

    fix unicode character usage

--- src/kdefrontend/dockwidgets/AxisDock.cpp
+++ src/kdefrontend/dockwidgets/AxisDock.cpp
@@ -330,7 +330,7 @@ void AxisDock::init() {
 	ui.cbLabelsFormat->addItem( i18n("Powers of 10") );
 	ui.cbLabelsFormat->addItem( i18n("Powers of 2") );
 	ui.cbLabelsFormat->addItem( i18n("Powers of e") );
-	ui.cbLabelsFormat->addItem( i18n("Multiples of \u03C0") );
+	ui.cbLabelsFormat->addItem( i18n("Multiples of π") );
 
 	m_initializing=false;
 }
