e2eb5eac440b701da1913aa6e2c92c12b270bff9
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2018-02-12 11:21:55 +0100

    fix activate Origin import test

--- tests/import_export/project/ProjectImportTest.cpp
+++ tests/import_export/project/ProjectImportTest.cpp
@@ -55,27 +55,26 @@ void ProjectImportTest::initTestCase() {
 //project tree of the file "origin8_test_tree_import.opj"
 /*
 test_tree_import\
+					\Book3
 					\Folder
-						\Book
+						\Book2
 							\Sheet1
 							\Sheet2
-						\MBook
+						\MBook2
 							\MSheet1
 							\MSheet2
 					\Folder1
-						\BookA
+						\MBook1
 							\Sheet1
-						\BookB
+						\Book1
 							\Sheet1
-						\Graph
-						\MBook
+						\Book4
 							\MSheet1
-					\BookC
+						\Graph2
+					\Excel1
 */
 
 void ProjectImportTest::testOrigin01() {
-	//TODO: activate this test once the crash with MBook2 is fixed
-	return;
 
 	//import the opj file into LabPlot's project object
 	OriginProjectParser parser;
@@ -85,82 +84,88 @@ void ProjectImportTest::testOrigin01() {
 
 	//check the project tree for the imported project
 
-	//first child of the root folde, folder "Folder" -> import into a Folder
+	//first child of the root folder, spreadsheet "Book3"
 	AbstractAspect* aspect = project.child<AbstractAspect>(0);
+	QCOMPARE(aspect != nullptr, true);
+	QCOMPARE(aspect->name(), QLatin1String("Book3"));
+	QCOMPARE(dynamic_cast<Spreadsheet*>(aspect) != nullptr, true);
+
+
+	//first child of the root folder, folder "Folder" -> import into a Folder
+	aspect = project.child<AbstractAspect>(1);
 	QCOMPARE(aspect != nullptr, true);
 	QCOMPARE(aspect->name(), QLatin1String("Folder"));
 	QCOMPARE(dynamic_cast<Folder*>(aspect) != nullptr, true);
 
-	//first child of "Folder", workbook "Book" with two sheets -> import into a Workbook with two Spreadsheets
-	aspect = project.child<AbstractAspect>(0)->child<AbstractAspect>(0);
+	//first child of "Folder", workbook "Book2" with two sheets -> import into a Workbook with two Spreadsheets
+	aspect = project.child<AbstractAspect>(1)->child<AbstractAspect>(0);
 	QCOMPARE(aspect != nullptr, true);
-	QCOMPARE(aspect->name(), QLatin1String("Book"));
+	QCOMPARE(aspect->name(), QLatin1String("Book2"));
 	QCOMPARE(dynamic_cast<Workbook*>(aspect) != nullptr, true);
 
-	aspect = project.child<AbstractAspect>(0)->child<AbstractAspect>(0)->child<AbstractAspect>(0);
+	aspect = project.child<AbstractAspect>(1)->child<AbstractAspect>(0)->child<AbstractAspect>(0);
 	QCOMPARE(aspect != nullptr, true);
 	QCOMPARE(aspect->name(), QLatin1String("Sheet1"));
 	QCOMPARE(dynamic_cast<Spreadsheet*>(aspect) != nullptr, true);
 
-	aspect = project.child<AbstractAspect>(0)->child<AbstractAspect>(0)->child<AbstractAspect>(1);
+	aspect = project.child<AbstractAspect>(1)->child<AbstractAspect>(0)->child<AbstractAspect>(1);
 	QCOMPARE(aspect != nullptr, true);
 	QCOMPARE(aspect->name(), QLatin1String("Sheet2"));
 	QCOMPARE(dynamic_cast<Spreadsheet*>(aspect) != nullptr, true);
 
 	//second child of "Folder", matrixbook "MBook" with two matrix sheets -> import into a Workbook with two Matrices
-	aspect = project.child<AbstractAspect>(0)->child<AbstractAspect>(1);
+	aspect = project.child<AbstractAspect>(1)->child<AbstractAspect>(1);
 	QCOMPARE(aspect != nullptr, true);
-	QCOMPARE(aspect->name(), QLatin1String("MBook"));
+	QCOMPARE(aspect->name(), QLatin1String("MBook2"));
 	QCOMPARE(dynamic_cast<Workbook*>(aspect) != nullptr, true);
 
-	aspect = project.child<AbstractAspect>(0)->child<AbstractAspect>(1)->child<AbstractAspect>(0);
+	aspect = project.child<AbstractAspect>(1)->child<AbstractAspect>(1)->child<AbstractAspect>(0);
 	QCOMPARE(aspect != nullptr, true);
 	QCOMPARE(aspect->name(), QLatin1String("MSheet1"));
 	QCOMPARE(dynamic_cast<Matrix*>(aspect) != nullptr, true);
 
-	aspect = project.child<AbstractAspect>(0)->child<AbstractAspect>(1)->child<AbstractAspect>(1);
+	aspect = project.child<AbstractAspect>(1)->child<AbstractAspect>(1)->child<AbstractAspect>(1);
 	QCOMPARE(aspect != nullptr, true);
 	QCOMPARE(aspect->name(), QLatin1String("MSheet2"));
 	QCOMPARE(dynamic_cast<Matrix*>(aspect) != nullptr, true);
 
 
 	//second child of the root folder, folder "Folder1" -> import into a Folder
-	aspect = project.child<AbstractAspect>(1);
+	aspect = project.child<AbstractAspect>(2);
 	QCOMPARE(aspect != nullptr, true);
 	QCOMPARE(aspect->name(), QLatin1String("Folder1"));
 	QCOMPARE(dynamic_cast<Folder*>(aspect) != nullptr, true);
 
-	//first child of "Folder1", worbook "BookA" with on sheet -> import into a Spreadsheet
-	aspect = project.child<AbstractAspect>(1)->child<AbstractAspect>(0);
+	//first child of "Folder1", matrix "MBook1"
+	aspect = project.child<AbstractAspect>(2)->child<AbstractAspect>(0);
 	QCOMPARE(aspect != nullptr, true);
-	QCOMPARE(aspect->name(), QLatin1String("Sheet1"));
+	QCOMPARE(aspect->name(), QLatin1String("MBook1"));
+	QCOMPARE(dynamic_cast<Matrix*>(aspect) != nullptr, true);
+
+	//second child of "Folder1", workbook "Book1" with on sheet -> import into a Spreadsheet
+	aspect = project.child<AbstractAspect>(2)->child<AbstractAspect>(1);
+	QCOMPARE(aspect != nullptr, true);
+	QCOMPARE(aspect->name(), QLatin1String("Book1"));
 	QCOMPARE(dynamic_cast<Spreadsheet*>(aspect) != nullptr, true);
 
-	//second child of "Folder1", worbook "BookB" with on sheet -> import into a Spreadsheet
-	aspect = project.child<AbstractAspect>(1)->child<AbstractAspect>(1);
+	//second child of "Folder1", workbook "Book4" with on sheet -> import into a Spreadsheet
+	aspect = project.child<AbstractAspect>(2)->child<AbstractAspect>(2);
 	QCOMPARE(aspect != nullptr, true);
-	QCOMPARE(aspect->name(), QLatin1String("Sheet1"));
+	QCOMPARE(aspect->name(), QLatin1String("Book4"));
 	QCOMPARE(dynamic_cast<Spreadsheet*>(aspect) != nullptr, true);
 
-	//third child of Folder, graph "Graph"-> import into a Worksheet
-	aspect = project.child<AbstractAspect>(1)->child<AbstractAspect>(2);
+	//third child of "Folder1", graph "Graph"-> import into a Worksheet
+	aspect = project.child<AbstractAspect>(2)->child<AbstractAspect>(3);
 	QCOMPARE(aspect != nullptr, true);
-	QCOMPARE(aspect->name(), QLatin1String("Graph"));
+	QCOMPARE(aspect->name(), QLatin1String("Graph2"));
 	QCOMPARE(dynamic_cast<Worksheet*>(aspect) != nullptr, true);
 	//TODO: check the created plot in the worksheet
 
-	//fourth child of "Folder1", matrixbook "MBook" with one sheet -> import into a Matrix
-	aspect = project.child<AbstractAspect>(1)->child<AbstractAspect>(3);
-	QCOMPARE(aspect != nullptr, true);
-	QCOMPARE(aspect->name(), QLatin1String("MSheet1"));
-	QCOMPARE(dynamic_cast<Matrix*>(aspect) != nullptr, true);
-
-
-	//third child of the root folder, workbook "BookC" with one sheet -> import into a Spreadsheet
-	aspect = project.child<AbstractAspect>(2);
-	QCOMPARE(aspect != nullptr, true);
-	QCOMPARE(aspect->name(), QLatin1String("Sheet1"));
-	QCOMPARE(dynamic_cast<Spreadsheet*>(aspect) != nullptr, true);
+	// TODO: loose window: spreadsheet "Excel1"
+	//aspect = project.child<AbstractAspect>(3);
+	//QCOMPARE(aspect != nullptr, true);
+	//QCOMPARE(aspect->name(), QLatin1String("Excel1"));
+	//QCOMPARE(dynamic_cast<Spreadsheet*>(aspect) != nullptr, true);
 }
 
 /*
@@ -196,7 +201,7 @@ void ProjectImportTest::testOrigin02() {
  * 3. check that both children are available after the second import
  */
 void ProjectImportTest::testOrigin03() {
-	//TODO: this test fails at the moment -> we need to implement this logic first
+	//TODO: fix import (second import creates a subfolder "Folder1")
 	return;
 
 	//import one single object
