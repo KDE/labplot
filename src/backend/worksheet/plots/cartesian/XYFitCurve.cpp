bd60c6e72665109bb5f6b8124a45c972bbe57a2a
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2018-02-25 19:48:28 +0100

    minor cmake fixes and fit parameter guess debugging

--- src/backend/worksheet/plots/cartesian/XYFitCurve.cpp
+++ src/backend/worksheet/plots/cartesian/XYFitCurve.cpp
@@ -112,21 +112,18 @@ void XYFitCurve::initStartValues(XYFitCurve::FitData& fitData, const XYCurve* cu
 	//double xmean = gsl_stats_mean(xVector->constData(), 1, tmpXDataColumn->rowCount());
 	double xmin = tmpXDataColumn->minimum();
 	double xmax = tmpXDataColumn->maximum();
-	double ymin = tmpYDataColumn->minimum();
-	double ymax = tmpYDataColumn->maximum();
+	//double ymin = tmpYDataColumn->minimum();
+	//double ymax = tmpYDataColumn->maximum();
 	double xrange = xmax-xmin;
 	//double yrange = ymax-ymin;
 	DEBUG("	x min/max = " << xmin << ' ' << xmax);
-	DEBUG("	y min/max = " << ymin << ' ' << ymax);
+	//DEBUG("	y min/max = " << ymin << ' ' << ymax);
 
 	switch (modelCategory) {
 	case nsl_fit_model_basic:
 		switch (modelType) {
 		case nsl_fit_model_polynomial:
-			// intersect
-			//TODO: paramStartValues[0] = xmin + fabs(ymax)/fabs(ymin)*(xmax-xmin)/2.;
-			//slope
-			//TODO: paramStartValues[1] = (ymax-ymin)/(xmax-xmin);
+			// not needed (works anyway)
 			break;
 		//TODO: handle basic models
 		case nsl_fit_model_power:
@@ -177,7 +174,7 @@ void XYFitCurve::initStartValues(XYFitCurve::FitData& fitData, const XYCurve* cu
 			paramStartValues[1] = (xmin+xmax)/2.;
 			paramStartValues[0] = xrange/10.;
 			break;
-		//TODO
+		//TODO: other types
 		default:
 			break;
 		}
