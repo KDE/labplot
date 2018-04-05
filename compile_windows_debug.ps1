d4669b38b575b2a218fcce8eb1e9b142302f7367
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2018-04-05 22:16:49 +0200

    updated Windows compile scripts

--- compile_windows_debug.ps1
+++ compile_windows_debug.ps1
@@ -1,6 +1,6 @@
 mkdir build
 cd build
 
-cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=C:\KDE  ..
+cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=C:\CraftRoot  ..
 
 mingw32-make -j 4 install
