c809ff283f78e96cf2e8dae3af9c59506f09037e
Pino Toscano <pino@kde.org>
2018-08-09 06:13:06 +0200

    i18n: ignore messages in 'tests' subdirs

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-$EXTRACTRC `find . -name \*.rc -o -name \*.ui` >> rc.cpp || exit 11
-$XGETTEXT `find . -name \*.h -o -name \*.c -o -name \*.cpp` -o $podir/labplot2.pot
+$EXTRACTRC `find . -name \*.rc -o -name \*.ui | grep -v '/tests/'` >> rc.cpp || exit 11
+$XGETTEXT `find . -name \*.h -o -name \*.c -o -name \*.cpp | grep -v '/tests/'` -o $podir/labplot2.pot
