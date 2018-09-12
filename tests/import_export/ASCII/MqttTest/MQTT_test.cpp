0ab99ce5345b39d85e243e8b0eda82da2629adb6
Alexander Semke <alexander.semke@web.de>
2018-09-12 09:06:04 +0200

    [krazy] added missing license headers.

--- tests/import_export/ASCII/MqttTest/MQTT_test.cpp
+++ tests/import_export/ASCII/MqttTest/MQTT_test.cpp
@@ -1,3 +1,32 @@
+/***************************************************************************
+File                 : MQTT_test.cpp
+Project              : LabPlot
+Description          : Tests for MQTT import.
+--------------------------------------------------------------------
+--------------------------------------------------------------------
+Copyright	: (C) 2018 Kovacs Ferencz (kferike98@gmail.com)
+
+***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor,                    *
+ *   Boston, MA  02110-1301  USA                                           *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "MQTT_test.h"
 #include "ui_MQTT_test.h"
 
