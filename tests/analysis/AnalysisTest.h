51c269618f3522afecba0c405dd316f956411fe3
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2018-09-15 13:33:31 +0200

    add missing files

---
+++ tests/analysis/AnalysisTest.h
@@ -0,0 +1,54 @@
+/***************************************************************************
+    File                 : AnalysisTest.h
+    Project              : LabPlot
+    Description          : Tests any analysis method
+    --------------------------------------------------------------------
+    Copyright            : (C) 2018 Stefan Gerlach (stefan.gerlach@uni.kn)
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor,                    *
+ *   Boston, MA  02110-1301  USA                                           *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef ANALYSISTEST_H
+#define ANALYSISTEST_H
+
+#include <QtTest>
+#include <backend/lib/macros.h>	// DEBUG()
+
+extern "C" {
+#include <gsl/gsl_math.h>
+}
+
+class AnalysisTest : public QObject {
+	Q_OBJECT
+
+private slots:
+	void initTestCase();
+protected:
+	// compare floats with given delta (could be useful for other tests too)
+	// delta - relative error
+	static inline void FuzzyCompare(double actual, double expected, double delta = 1.e-12) {
+		if (fabs(expected) < delta)
+			QVERIFY(fabs(actual) < delta);
+		else {
+			DEBUG(std::setprecision(15) << actual - fabs(actual)*delta << " <= " << expected << " <= " << actual + fabs(actual)*delta);
+			QVERIFY(!gsl_fcmp(actual, expected, delta));
+		}
+	}
+};
+#endif
