25a8be78aa4956e4a479604b40c5b7e2b25ff66f
Alexander Semke <alexander.semke@web.de>
2018-09-16 09:25:43 +0200

    Incresed the minimum Qt verion to 5.6 in order to use high dpi scaling on default.

--- src/kdefrontend/LabPlot.cpp
+++ src/kdefrontend/LabPlot.cpp
@@ -50,6 +50,8 @@
 #include "backend/lib/macros.h"
 
 int main (int argc, char *argv[]) {
+	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+	QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 	QApplication app(argc, argv);
 	KLocalizedString::setApplicationDomain("labplot2");
 
