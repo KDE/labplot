c9c7d674d6b4011cd6c54b1b77970c84e2beffa9
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2018-11-14 22:55:43 +0100

    [matrix] code clean up

--- src/backend/matrix/MatrixModel.h
+++ src/backend/matrix/MatrixModel.h
@@ -37,44 +37,44 @@ class Matrix;
 class MatrixModel : public QAbstractItemModel {
 	Q_OBJECT
 
-	public:
-		explicit MatrixModel(Matrix*);
+public:
+	explicit MatrixModel(Matrix*);
 
-		//! \name Overloaded functions from QAbstractItemModel
-		//@{
-		Qt::ItemFlags flags(const QModelIndex&) const override;
-		QVariant data(const QModelIndex& index, int role) const override;
-		QVariant headerData(int section, Qt::Orientation orientation,int role = Qt::DisplayRole) const override;
-		int rowCount(const QModelIndex& parent = QModelIndex()) const override;
-		int columnCount(const QModelIndex& parent = QModelIndex()) const override;
-		bool setData(const QModelIndex& index, const QVariant& value, int role) override;
-		QModelIndex index(int row, int column, const QModelIndex& parent = QModelIndex()) const override;
-		QModelIndex parent(const QModelIndex& child) const override;
-		//@}
+	//! \name Overloaded functions from QAbstractItemModel
+	//@{
+	Qt::ItemFlags flags(const QModelIndex&) const override;
+	QVariant data(const QModelIndex& index, int role) const override;
+	QVariant headerData(int section, Qt::Orientation orientation,int role = Qt::DisplayRole) const override;
+	int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+	int columnCount(const QModelIndex& parent = QModelIndex()) const override;
+	bool setData(const QModelIndex& index, const QVariant& value, int role) override;
+	QModelIndex index(int row, int column, const QModelIndex& parent = QModelIndex()) const override;
+	QModelIndex parent(const QModelIndex& child) const override;
+	//@}
 
-		void updateHeader();
-		void setSuppressDataChangedSignal(bool);
-		void setChanged();
+	void updateHeader();
+	void setSuppressDataChangedSignal(bool);
+	void setChanged();
 
-	private slots:
-		void handleColumnsAboutToBeInserted(int before, int count);
-		void handleColumnsInserted(int first, int count);
-		void handleColumnsAboutToBeRemoved(int first, int count);
-		void handleColumnsRemoved(int first, int count);
-		void handleRowsAboutToBeInserted(int before, int count);
-		void handleRowsInserted(int first, int count);
-		void handleRowsAboutToBeRemoved(int first, int count);
-		void handleRowsRemoved(int first, int count);
-		void handleDataChanged(int top, int left, int bottom, int right);
-		void handleCoordinatesChanged();
-		void handleFormatChanged();
+private slots:
+	void handleColumnsAboutToBeInserted(int before, int count);
+	void handleColumnsInserted(int first, int count);
+	void handleColumnsAboutToBeRemoved(int first, int count);
+	void handleColumnsRemoved(int first, int count);
+	void handleRowsAboutToBeInserted(int before, int count);
+	void handleRowsInserted(int first, int count);
+	void handleRowsAboutToBeRemoved(int first, int count);
+	void handleRowsRemoved(int first, int count);
+	void handleDataChanged(int top, int left, int bottom, int right);
+	void handleCoordinatesChanged();
+	void handleFormatChanged();
 
-	private:
-		Matrix* m_matrix;
-		bool m_suppressDataChangedSignal;
+private:
+	Matrix* m_matrix;
+	bool m_suppressDataChangedSignal{false};
 
-	signals:
-		void changed();
+signals:
+	void changed();
 };
 
 #endif
