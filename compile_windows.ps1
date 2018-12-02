e706d78a9a427abf39b48311eea3fb4d3bd0d783
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2018-12-02 13:33:19 +0100

    minor improvement

--- compile_windows.ps1
+++ compile_windows.ps1
@@ -7,3 +7,5 @@ cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=C:\
 #cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=C:\CraftRoot  ..
 
 mingw32-make -j 4 install
+
+cd ..
