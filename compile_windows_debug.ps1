1beb1f16bb67d9497a476f2492ea7ab744e2a68b
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2018-12-02 13:10:56 +0100

    [windows] remove debug compile script

--- compile_windows_debug.ps1
+++
@@ -1,8 +0,0 @@
-$BUILDDIR = "build_debug"
-
-mkdir $BUILDDIR
-cd $BUILDDIR
-
-cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=C:\CraftRoot  ..
-
-mingw32-make -j 4 install
