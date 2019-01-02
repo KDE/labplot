00d66a5527b7a7ec08f5339359afc89657709d8a
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2019-01-02 19:45:04 +0100

    [nsl] fix cppcheck warnings in tests

--- src/backend/nsl/nsl_dft_test.c
+++ src/backend/nsl/nsl_dft_test.c
@@ -29,15 +29,20 @@
 #include <stdio.h>
 #include "nsl_dft.h"
 
+#define NN 10
+#define TWOSIDED 1	/* 0,1 */
+
 int main() {
 	double data[]={1, 1, 3, 3, 1, -1, 0, 1, 1, 0};
 	/*double data[]={1, 2, 3, 3, 1};*/
 	const int N=10;
-	const int two_sided=1;
 
+#if TWOSIDED == 1
+	int size=N;
+#else
 	int size=N/2;
-	if(two_sided)
-		size=N;
+#endif
+
 	/* input */
 	int i;
 	for(i=0; i < N; i++)
@@ -45,7 +50,7 @@ int main() {
 	puts("\nraw:");
 
 	// 'raw' result
-	nsl_dft_transform(data, 1, N, two_sided, nsl_dft_result_raw);
+	nsl_dft_transform(data, 1, N, TWOSIDED, nsl_dft_result_raw);
 
 	/* output */
 	for(i=0; i < size; i++)
@@ -54,7 +59,7 @@ int main() {
 
 	double data2[]={1, 1, 3, 3, 1, -1, 0, 1, 1, 0};
 	/*double data2[]={1, 2, 3, 3, 1};*/
-	nsl_dft_transform(data2, 1, N, two_sided, nsl_dft_result_real);
+	nsl_dft_transform(data2, 1, N, TWOSIDED, nsl_dft_result_real);
 
 	/* output */
 	for(i=0; i < size; i++)
@@ -63,7 +68,7 @@ int main() {
 
 	double data3[]={1, 1, 3, 3, 1, -1, 0, 1, 1, 0};
 	/*double data3[]={1, 2, 3, 3, 1};*/
-	nsl_dft_transform(data3, 1, N, two_sided, nsl_dft_result_imag);
+	nsl_dft_transform(data3, 1, N, TWOSIDED, nsl_dft_result_imag);
 
 	/* output */
 	for(i=0; i < size; i++)
@@ -72,7 +77,7 @@ int main() {
 
 	double data4[]={1, 1, 3, 3, 1, -1, 0, 1, 1, 0};
 	/*double data4[]={1, 2, 3, 3, 1};*/
-	nsl_dft_transform(data4, 1, N, two_sided, nsl_dft_result_magnitude);
+	nsl_dft_transform(data4, 1, N, TWOSIDED, nsl_dft_result_magnitude);
 
 	/* output */
 	for(i=0; i < size; i++)
@@ -81,7 +86,7 @@ int main() {
 
 	double data5[]={1, 1, 3, 3, 1, -1, 0, 1, 1, 0};
 	/*double data5[]={1, 2, 3, 3, 1};*/
-	nsl_dft_transform(data5, 1, N, two_sided, nsl_dft_result_amplitude);
+	nsl_dft_transform(data5, 1, N, TWOSIDED, nsl_dft_result_amplitude);
 
 	/* output */
 	for(i=0; i < size; i++)
@@ -90,7 +95,7 @@ int main() {
 
 	double data6[]={1, 1, 3, 3, 1, -1, 0, 1, 1, 0};
 	/*double data6[]={1, 2, 3, 3, 1};*/
-	nsl_dft_transform(data6, 1, N, two_sided, nsl_dft_result_power);
+	nsl_dft_transform(data6, 1, N, TWOSIDED, nsl_dft_result_power);
 
 	/* output */
 	for(i=0; i < size; i++)
@@ -99,7 +104,7 @@ int main() {
 
 	double data7[]={1, 1, 3, 3, 1, -1, 0, 1, 1, 0};
 	/*double data7[]={1, 2, 3, 3, 1};*/
-	nsl_dft_transform(data7, 1, N, two_sided, nsl_dft_result_phase);
+	nsl_dft_transform(data7, 1, N, TWOSIDED, nsl_dft_result_phase);
 
 	/* output */
 	for(i=0; i < size; i++)
@@ -108,7 +113,7 @@ int main() {
 
 	double data8[]={1, 1, 3, 3, 1, -1, 0, 1, 1, 0};
 	/*double data8[]={1, 2, 3, 3, 1};*/
-	nsl_dft_transform(data8, 1, N, two_sided, nsl_dft_result_dB);
+	nsl_dft_transform(data8, 1, N, TWOSIDED, nsl_dft_result_dB);
 
 	/* output */
 	for(i=0; i < size; i++)
@@ -116,7 +121,7 @@ int main() {
 	puts("\nsquare magnitude:");
 
 	double data9[]={1, 1, 3, 3, 1, -1, 0, 1, 1, 0};
-	nsl_dft_transform(data9, 1, N, two_sided, nsl_dft_result_squaremagnitude);
+	nsl_dft_transform(data9, 1, N, TWOSIDED, nsl_dft_result_squaremagnitude);
 
 	/* output */
 	for(i=0; i < size; i++)
@@ -124,7 +129,7 @@ int main() {
 	puts("\nsquare amplitude:");
 
 	double data10[]={1, 1, 3, 3, 1, -1, 0, 1, 1, 0};
-	nsl_dft_transform(data10, 1, N, two_sided, nsl_dft_result_squareamplitude);
+	nsl_dft_transform(data10, 1, N, TWOSIDED, nsl_dft_result_squareamplitude);
 
 	/* output */
 	for(i=0; i < size; i++)
@@ -132,7 +137,7 @@ int main() {
 	puts("\nnormdB:");
 
 	double data11[]={1, 1, 3, 3, 1, -1, 0, 1, 1, 0};
-	nsl_dft_transform(data11, 1, N, two_sided, nsl_dft_result_normdB);
+	nsl_dft_transform(data11, 1, N, TWOSIDED, nsl_dft_result_normdB);
 
 	/* output */
 	for(i=0; i < size; i++)
--- src/backend/nsl/nsl_smooth_mal_test.c
+++ src/backend/nsl/nsl_smooth_mal_test.c
@@ -39,6 +39,7 @@ int main() {
 	status = nsl_smooth_moving_average_lagged(data, 9, points, weight, nsl_smooth_pad_none);
 	for(i=0;i<9;i++)
 		printf(" %g",data[i]);
+	printf("	status = %d\n", status);
 	/*printf("pad_interp\n");
 	status = nsl_smooth_moving_average(data, 9, points, weight, nsl_smooth_pad_interp);
 	*/
@@ -47,20 +48,24 @@ int main() {
 	status = nsl_smooth_moving_average_lagged(data2, 9, points, weight, nsl_smooth_pad_mirror);
 	for(i=0;i<9;i++)
 		printf(" %g",data2[i]);
+	printf("	status = %d\n", status);
 	printf("\npad_nearest\n");
 	double data3[9]={2,2,5,2,1,0,1,4,9};
 	status = nsl_smooth_moving_average_lagged(data3, 9, points, weight, nsl_smooth_pad_nearest);
 	for(i=0;i<9;i++)
 		printf(" %g",data3[i]);
+	printf("	status = %d\n", status);
 	printf("\npad_constant\n");
 	double data4[9]={2,2,5,2,1,0,1,4,9};
 	status = nsl_smooth_moving_average_lagged(data4, 9, points, weight, nsl_smooth_pad_constant);
 	for(i=0;i<9;i++)
 		printf(" %g",data4[i]);
+	printf("	status = %d\n", status);
 	printf("\npad_periodic\n");
 	double data5[9]={2,2,5,2,1,0,1,4,9};
 	status = nsl_smooth_moving_average_lagged(data5, 9, points, weight, nsl_smooth_pad_periodic);
 	for(i=0;i<9;i++)
 		printf(" %g",data5[i]);
+	printf("	status = %d\n", status);
 	puts("");
 }
