ef7aa11fffdbfeeb1c43359d2b39a31df020f332
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2019-01-20 17:49:21 +0100

    [nsl] accelerate tests

--- tests/nsl/sf_basic/NSLSFBasicTest.cpp
+++ tests/nsl/sf_basic/NSLSFBasicTest.cpp
@@ -47,7 +47,7 @@ void NSLSFBasicTest::testlog2_int_C99() {
 }
 
 void NSLSFBasicTest::testlog2_int() {
-	for (unsigned int i = 1; i < 1e6; i++) {
+	for (unsigned int i = 1; i < 1e5; i++) {
 		int result = nsl_sf_log2_int(i);
 		QCOMPARE(result, (int)log2(i));
 	}
@@ -59,7 +59,7 @@ void NSLSFBasicTest::testlog2_int() {
 }
 void NSLSFBasicTest::testlog2_longlong() {
 #ifndef _MSC_VER	/* not implemented yet */
-	for (unsigned long long i = 1; i < 1e6; i++) {
+	for (unsigned long long i = 1; i < 1e5; i++) {
 		int result = nsl_sf_log2_longlong(i);
 		QCOMPARE(result, (int)log2(i));
 	}
@@ -73,7 +73,7 @@ void NSLSFBasicTest::testlog2_longlong() {
 
 
 void NSLSFBasicTest::testlog2_int2() {
-	for (int i = 1; i < 1e6; i++) {
+	for (int i = 1; i < 1e5; i++) {
 		int result = nsl_sf_log2_int2(i);
 		QCOMPARE(result, (int)log2(i));
 	}
@@ -85,7 +85,7 @@ void NSLSFBasicTest::testlog2_int2() {
 }
 
 void NSLSFBasicTest::testlog2_int3() {
-	for (unsigned int i = 1; i < 1e6; i++) {
+	for (unsigned int i = 1; i < 1e5; i++) {
 		int result = nsl_sf_log2_int3(i);
 		QCOMPARE(result, (int)log2(i));
 	}
@@ -97,7 +97,7 @@ void NSLSFBasicTest::testlog2_int3() {
 }
 
 void NSLSFBasicTest::testlog2p1_int() {
-	for (int i = 1; i < 1e6; i++) {
+	for (int i = 1; i < 1e5; i++) {
 		int result = nsl_sf_log2p1_int(i);
 		QCOMPARE(result, (int)log2(i) + 1);
 	}
