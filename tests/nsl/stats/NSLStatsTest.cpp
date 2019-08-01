37e6ee4a50a494f9c4f798f6f86d999a305866ed
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2019-08-01 23:30:53 +0200

    minor code style fixes

--- tests/nsl/stats/NSLStatsTest.cpp
+++ tests/nsl/stats/NSLStatsTest.cpp
@@ -41,7 +41,7 @@ const int NQ = 13;
 void NSLStatsTest::testQuantile() {
 	const double data_sorted[] = {1, 1, 1, 3, 4, 7, 9, 11, 13, 13};
 	double data_unsorted[] = {3, 7, 11, 1, 13, 1, 9, 1, 13, 4};
-	const double quantile[]={.0, .1, .2, .25, .3, .4, .5, .6, .7, .75, .8, .9, 1.};
+	const double quantile[] = {.0, .1, .2, .25, .3, .4, .5, .6, .7, .75, .8, .9, 1.};
 	const double result[NSL_STATS_QUANTILE_TYPE_COUNT][NQ+1] = {
 		{1., 1., 1., 1., 1., 3., 4., 7., 9., 11., 11., 13., 13., 4.},
 		{1., 1., 1., 2., 2., 3.5, 5.5, 8., 10., 12., 12., 13., 13., 5.5},
@@ -53,26 +53,24 @@ void NSLStatsTest::testQuantile() {
 		{1., 1., 1., 1., 28./15., 52./15., 5.5, 121./15., 152./15., 335./30., 12.2, 13., 13., 5.5},
 		{1., 1., 1., 1., 1.9, 3.475, 5.5, 8.05, 10.1, 11.125, 12.15, 13., 13., 5.5}};
 
-	int i, j;
-	for (i = 1;i <= NSL_STATS_QUANTILE_TYPE_COUNT; i++) {
-		printf("quantile type %d\n", i);
-		nsl_stats_quantile_type type = (nsl_stats_quantile_type)i;
-		for (j = 0;j < NQ; ++j) {
-			double value = nsl_stats_quantile_sorted(data_sorted, 1, N, quantile[j], type);
+	int type, i;
+	for (type = 1; type <= NSL_STATS_QUANTILE_TYPE_COUNT; ++type) {
+		printf("quantile type %d\n", type);
+		for (i = 0; i < NQ; ++i) {
+			double value = nsl_stats_quantile_sorted(data_sorted, 1, N, quantile[i], (nsl_stats_quantile_type)type);
 			//printf("%d %d: %g %g\n", i, j, value, result[i-1][j]);
-			QCOMPARE(value, result[i-1][j]);
+			QCOMPARE(value, result[type-1][i]);
 		}
-		QCOMPARE(nsl_stats_median_sorted(data_sorted, 1, N, type), result[i-1][NQ]);
+		QCOMPARE(nsl_stats_median_sorted(data_sorted, 1, N, (nsl_stats_quantile_type)type), result[type-1][NQ]);
 	}
-	for (i = 1;i <= NSL_STATS_QUANTILE_TYPE_COUNT; ++i) {
-		printf("quantile type %d\n", i);
-		nsl_stats_quantile_type type = (nsl_stats_quantile_type)i;
-		for (j = 0;j < NQ; ++j) {
-			double value = nsl_stats_quantile(data_unsorted, 1, N, quantile[j], type);
+	for (type = 1; type <= NSL_STATS_QUANTILE_TYPE_COUNT; ++type) {
+		printf("quantile type %d\n", type);
+		for (i = 0; i < NQ; ++i) {
+			double value = nsl_stats_quantile(data_unsorted, 1, N, quantile[i], (nsl_stats_quantile_type)type);
 			//printf("%d %d: %g %g\n", i, j, value, result[i-1][j]);
-			QCOMPARE(value, result[i-1][j]);
+			QCOMPARE(value, result[type-1][i]);
 		}
-		QCOMPARE(nsl_stats_median_sorted(data_sorted, 1, N, type), result[i-1][NQ]);
+		QCOMPARE(nsl_stats_median_sorted(data_sorted, 1, N, (nsl_stats_quantile_type)type), result[type-1][NQ]);
 	}
 }
 
