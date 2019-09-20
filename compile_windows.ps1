15ffa411fc2e1cf8e280a1ad635f3ea4260bedf7
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2019-09-20 06:41:01 +0200

    GIT_SILENT [windows] extend build script

--- compile_windows.ps1
+++ compile_windows.ps1
@@ -4,6 +4,7 @@ mkdir $BUILDDIR
 cd $BUILDDIR
 
 cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=C:\CraftRoot  ..
+#cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DENABLE_TESTS=OFF -DCMAKE_INSTALL_PREFIX=C:\CraftRoot  ..
 #cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=C:\CraftRoot  ..
 
 mingw32-make -j 4 install
