f7e92e22ed7d9b13c1581d76b431ebdac1a3e902
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2020-01-12 11:18:51 +0100

    Fix building with MSVC

--- compile_windows_msvc.ps1
+++ compile_windows_msvc.ps1
@@ -3,6 +3,6 @@ $BUILDDIR = "build_msvc"
 mkdir $BUILDDIR
 cd $BUILDDIR
 
-cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release  ..
+cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-DH5_BUILT_AS_DYNAMIC_LIB" ..
 
 jom.exe
