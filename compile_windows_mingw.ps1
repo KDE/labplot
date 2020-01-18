87e1abc5458782285d36811520282e8ff93fcb2d
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2020-01-19 00:04:22 +0100

    Update Windows compile scripts

--- compile_windows_mingw.ps1
+++ compile_windows_mingw.ps1
@@ -1,4 +1,4 @@
-$BUILDDIR = "build"
+$BUILDDIR = "build_mingw"
 
 mkdir $BUILDDIR
 cd $BUILDDIR
@@ -7,6 +7,6 @@ cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=C:\
 #cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DENABLE_TESTS=OFF -DCMAKE_INSTALL_PREFIX=C:\CraftRoot  ..
 #cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=C:\CraftRoot  ..
 
-mingw32-make -j 4 install
+mingw32-make -j 16 install
 
 cd ..
