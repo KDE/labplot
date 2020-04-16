5b19352a77c60a823145f5fb99fe4dacc96f7f1a
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2020-04-16 18:44:19 +0200

    GIT_SILENT Windows build script updated

--- compile_windows_msvc.ps1
+++ compile_windows_msvc.ps1
@@ -4,6 +4,8 @@ mkdir $BUILDDIR
 cd $BUILDDIR
 
 cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release ..
+#cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Debug ..
+#cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Debug -DENABLE_TESTS=OFF ..
 
 jom.exe install
 
