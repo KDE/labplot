cd148ff1a556626bd8b4b6a4db0d56dd9083df58
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2020-06-03 23:39:23 +0200

    Fix previous commit

--- src/kdefrontend/dockwidgets/ColumnDock.cpp
+++ src/kdefrontend/dockwidgets/ColumnDock.cpp
@@ -368,7 +368,7 @@ void ColumnDock::columnFormatChanged() {
 	case AbstractColumn::ColumnMode::Day:
 	case AbstractColumn::ColumnMode::DateTime: {
 		auto* filter = static_cast<DateTime2StringFilter*>(m_column->outputFilter());
-		DEBUG(filter->format())
+		DEBUG(STDSTRING(filter->format()))
 		ui.cbDateTimeFormat->setCurrentText(filter->format());
 		break;
 	}
