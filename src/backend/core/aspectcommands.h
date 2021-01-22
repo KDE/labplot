e39878ae58f5d3add84e8837662631606f6008f4
Alexander Semke <alexander.semke@web.de>
2021-01-22 09:04:27 +0100

    When deleting an aspect, emit the "about to be removed" signal also for all its children. This allows to remove the "bad hack" in XYCurve to be aware of deleted columns when the parent spreadsheet is being deleted. This is also more generic now and applies for other "curves" like the histogram, too. So, the histogram is now also properly reacting of spreadsheet or workbook deletions.

--- src/backend/core/aspectcommands.h
+++ src/backend/core/aspectcommands.h
@@ -57,6 +57,11 @@ public:
 			nextSibling = m_target->m_children.at(m_target->indexOfChild(m_child) + 1);
 
 		emit m_target->q->aspectAboutToBeRemoved(m_child);
+
+		//emit the "about to be removed" signal also for all children of the aspect being currently removed
+		for (auto* child : m_child->children<AbstractAspect>(AbstractAspect::ChildIndexFlag::Recursive | AbstractAspect::ChildIndexFlag::IncludeHidden))
+			m_child->aspectAboutToBeRemoved(child);
+
 		m_index = m_target->removeChild(m_child);
 		emit m_target->q->aspectRemoved(m_target->q, nextSibling, m_child);
 //		m_removed = true;
