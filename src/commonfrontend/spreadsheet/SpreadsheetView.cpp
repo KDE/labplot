5b4e8b488d2ea19d1a7dd8e2c364e5a204b32cee
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2021-02-06 15:57:06 +0100

    Fix LaTeX export of large tables (MR 16)

--- src/commonfrontend/spreadsheet/SpreadsheetView.cpp
+++ src/commonfrontend/spreadsheet/SpreadsheetView.cpp
@@ -3516,7 +3516,7 @@ void SpreadsheetView::exportToLaTeX(const QString & path, const bool exportHeade
 					rowCount++;
 					if (rowCount == maxRows) {
 						out << endTabularTable;
-						out << QLatin1String("\\newpage \n");
+						out << QLatin1String("\\clearpage \n");
 
 						if (captions)
 							if (!captionRemoved)
@@ -3650,7 +3650,7 @@ void SpreadsheetView::exportToLaTeX(const QString & path, const bool exportHeade
 				rowCount++;
 				if (rowCount == maxRows) {
 					out << endTabularTable;
-					out << QLatin1String("\\newpage \n");
+					out << QLatin1String("\\clearpage \n");
 					if (captions)
 						if (!captionRemoved)
 							textable.removeAt(1);
