8338bef53afc3e5c8afc6dee1661b664e874affa
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2021-03-08 17:32:44 +0100

    Implement auto tick count for axes

--- tests/backend/Range/RangeTest.cpp
+++ tests/backend/Range/RangeTest.cpp
@@ -1,9 +1,9 @@
 /***************************************************************************
-    File                 : ParserTest.cpp
+    File                 : RangeTest.cpp
     Project              : LabPlot
-    Description          : Tests for the Parser
+    Description          : Tests for Range
     --------------------------------------------------------------------
-    Copyright            : (C) 2020 Stefan Gerlach (stefan.gerlach@uni.kn)
+    Copyright            : (C) 2021 Stefan Gerlach (stefan.gerlach@uni.kn)
  ***************************************************************************/
 
 /***************************************************************************
@@ -25,62 +25,27 @@
  *                                                                         *
  ***************************************************************************/
 
-#include "ParserTest.h"
-
-extern "C" {
-#include "backend/gsl/parser.h"
-}
-
-#include <gsl/gsl_errno.h>
-#include <gsl/gsl_const_mksa.h>
+#include "RangeTest.h"
+#include "backend/lib/Range.h"
 
 //**********************************************************
 //****************** Function tests ************************
 //**********************************************************
 
-void ParserTest::testBasics() {
-	const QVector<QPair<QString, double>> tests{ 
-		{"42", 42.}, {"1.", 1.}, {"1+1", 2.}, {"1+2+3+4+5", 15.}, {"2*3", 6.}, { "3/2", 1.5}, {"2 -4 +6 -1 -1- 0 +8", 10.},
-		{"1/3+1/3+1/3", 1.}, {"1.5 + 2.5", 4.}, {"4*2.5 + 8.5+1.5 / 3.0", 19.}, {"5.0005 + 0.0095", 5.01},
-		{"pi", M_PI}, {"e", M_E}, {"e^1", M_E},  {"hbar", GSL_CONST_MKSA_PLANCKS_CONSTANT_HBAR},
-		{" 1   +   1  ", 2.}, {"1-    2", -1.}, {"2*    2.5", 5.}, {"3 + 8/5 -1 -2*5", -6.4},
-		{"(1)", 1.}, {"-(1)", -1.}, {"(1+1)", 2}, {"(sin(0))", 0.} ,{"(( ((2)) + 4))*((5))", 30.},
-		{"2^2", 4.}, {"3**2", 9.}, {"1%1", 0.}, {"3%2", 1.},
-		{"1.e-5", 1.e-5}, {"9.5E3", 9500.}, {"|1.5|", 1.5}, {"|-2.5|", 2.5}, {"0!", 1}, {"4!", 24}, {"-3!", -6.},
-		{"exp(0)", 1.}, {"exp(1)", M_E}, {"sqrt(0)", 0.}, {"sin(0)", 0.}, {"cos(pi)", -1.}
+void RangeTest::testTickCount() {
+	const QVector<QPair<Range<double>, int>> tests{
+		{{0., 1.}, 6}, {{0., 0.01}, 6}, {{0., 100.}, 6},
+		{{0., 2.}, 5}, {{0., 3.}, 7}, {{0., 4.}, 5}, {{0., 5.}, 6},
+		{{0., 6.}, 7}, {{0., 7.}, 8}, {{0., 8.}, 9}, {{0., 9.}, 10},
+		{{1.6, 2.2}, 7}, {{1.3, 2.7}, 8}, {{1., 30.}, 7}
 	};
-
-	for ( auto& expr: tests)
-		QCOMPARE(parse(qPrintable(expr.first), "C"), expr.second);
-
-	const QVector<QPair<QString, double>> testsFuzzy{ 
-		{"(sin(pi))", 0.}
-	};
-
-	for ( auto& expr: testsFuzzy)
-		FuzzyCompare(parse(qPrintable(expr.first), "C"), expr.second, 1.e-15);
+	for ( auto& test: tests) {
+		DEBUG(test.second);
+		QCOMPARE(test.first.autoTickCount(), test.second);
+	}
 }
 
-void ParserTest::testErrors() {
-	gsl_set_error_handler_off();	// do not crash
-
-	const QVector<QString> testsNan{
-		"", "a", "1+", "a+1", "&", "%", "+", "*", "/", "{1}", "{1*2}", "(1+1))", "a/0", "0/0", "1/0 + a",
-		"sqrt(-1)", "log(-1)", "log(0)", "asin(2)"
-	};
-
-	for ( auto& expr: testsNan)
-		QVERIFY(qIsNaN(parse(qPrintable(expr), "C")));
-	
-	const QVector<QString> testsInf{
-		"1/0", "-1/0",  "1+1/0"
-	};
-
-	for ( auto& expr: testsInf)
-		QVERIFY(qIsInf(parse(qPrintable(expr), "C")));
-}
-
-void ParserTest::testVariables() {
+/*void ParserTest::testVariables() {
 	assign_symbol("a", 1.);
 	const QVector<QPair<QString, double>> tests{ 
 		{"a", 1.}, {"a+1", 2.}, {"a+1.5", 2.5}, {"a!", 1.}
@@ -144,6 +109,6 @@ void ParserTest::testPerformance2() {
 			QCOMPARE(parse("sin(alpha)^2 + cos(alpha)^2", "C"), 1.);
 		}
 	}
-}
+}*/
 
-QTEST_MAIN(ParserTest)
+QTEST_MAIN(RangeTest)
