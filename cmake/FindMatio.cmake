370c8f33a8e5279fcbca6e1caba4007351cb2860
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2021-05-01 12:20:21 +0200

    matio: cmake find

---
+++ cmake/FindMatio.cmake
@@ -0,0 +1,57 @@
+#=============================================================================
+# Copyright (c) 2021 Stefan Gerlach <stefan.gerlach@uni.kn>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#=============================================================================
+
+find_library(MATIO_LIBRARY matio)
+
+find_path(MATIO_INCLUDE_DIR matio.h)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Matio
+    FOUND_VAR
+    	MATIO_FOUND
+    REQUIRED_VARS
+        MATIO_LIBRARY
+	MATIO_INCLUDE_DIR
+)
+
+if(MATIO_FOUND)
+   add_library(matio UNKNOWN IMPORTED)
+    set_target_properties(matio PROPERTIES
+	    IMPORTED_LOCATION "${MATIO_LIBRARY}"
+	    INTERFACE_INCLUDE_DIRECTORIES "${MATIO_INCLUDE_DIR}"
+    )
+else()
+   set(MATIO_LIBRARY "")
+endif()
+
+mark_as_advanced(MATIO_LIBRARY MATIO_INCLUDE_DIR)
+
+include(FeatureSummary)
+set_package_properties(Matio PROPERTIES
+    DESCRIPTION "Reading and writing binary MATLAB MAT files"
+    URL "https://github.com/tbeu/matio"
+)
