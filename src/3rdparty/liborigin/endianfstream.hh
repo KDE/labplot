61e60115f60cab58fd8e6d3d8013ddb7aaa8a30d
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-09-01 07:56:02 +0200

    Manually convert remaining GPL-2.0-or-later license statements

--- src/3rdparty/liborigin/endianfstream.hh
+++ src/3rdparty/liborigin/endianfstream.hh
@@ -1,30 +1,11 @@
-/***************************************************************************
-	File                 : endianfstream.hh
-	--------------------------------------------------------------------
-	Copyright            : (C) 2008 Alex Kargovsky
-						   Email (use @ for *)  : kargovsky*yumr.phys.msu.su
-	Description          : Endianless file stream class
+/*
+    File                 : endianfstream.hh
+    --------------------------------------------------------------------
+    Description          : Endianless file stream class
 
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *  This program is free software; you can redistribute it and/or modify   *
- *  it under the terms of the GNU General Public License as published by   *
- *  the Free Software Foundation; either version 2 of the License, or      *
- *  (at your option) any later version.                                    *
- *                                                                         *
- *  This program is distributed in the hope that it will be useful,        *
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
- *  GNU General Public License for more details.                           *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the Free Software           *
- *   Foundation, Inc., 51 Franklin Street, Fifth Floor,                    *
- *   Boston, MA  02110-1301  USA                                           *
- *                                                                         *
- ***************************************************************************/
+    SPDX-FileCopyrightText: 2008 Alex Kargovsky <kargovsky@yumr.phys.msu.su>
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
 
 #ifndef ENDIAN_FSTREAM_H
 #define ENDIAN_FSTREAM_H
