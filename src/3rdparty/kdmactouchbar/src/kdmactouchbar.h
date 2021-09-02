c4a22581a82f613d1dc37f1170369208209452b5
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-09-02 07:48:34 +0200

    Convert LGPL-3.0-or-later license headers to SPDX

--- src/3rdparty/kdmactouchbar/src/kdmactouchbar.h
+++ src/3rdparty/kdmactouchbar/src/kdmactouchbar.h
@@ -1,21 +1,8 @@
 /*
-    SPDX-FileCopyrightText: 2019-2020 Klaralvdalens Datakonsult AB a KDAB Group company, info@kdab.com.
-    All rights reserved.
-
     This file is part of the KD MacTouchBar library.
+    SPDX-FileCopyrightText: 2019-2020 Klaralvdalens Datakonsult AB a KDAB Group company <info@kdab.com>
 
-    This file may be distributed and/or modified under the terms of the
-    GNU Lesser General Public License version 3 as published by the
-    Free Software Foundation and appearing in the file LICENSE.LGPL.txt included.
-
-    You may even contact us at info@kdab.com for different licensing options.
-
-    This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-    WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-
-    Contact info@kdab.com if any conditions of this licensing are not
-    clear to you.
-
+    SPDX-License-Identifier: LGPL-3.0-or-later
 */
 
 #ifndef KDMACTOUCHBAR_H
