a475e3cfbaa29280f447f56c150333184effee17
Martin Marmsoler <martin.marmsoler@gmail.com>
2021-12-17 12:24:25 +0100

    Image: remove retransform from scaleimage. So it will not be called during load

--- src/backend/worksheet/Image.cpp
+++ src/backend/worksheet/Image.cpp
@@ -165,18 +165,22 @@ void Image::setOpacity(qreal opacity) {
 		exec(new ImageSetOpacityCmd(d, opacity, ki18n("%1: set border opacity")));
 }
 
-STD_SETTER_CMD_IMPL_F_S(Image, SetWidth, int, width, scaleImage)
+STD_SETTER_CMD_IMPL_F_S(Image, SetWidth, int, width, retransform)
 void Image::setWidth(int width) {
 	Q_D(Image);
-	if (width != d->width)
+	if (width != d->width) {
 		exec(new ImageSetWidthCmd(d, width, ki18n("%1: set width")));
+		d->scaleImage();
+	}
 }
 
-STD_SETTER_CMD_IMPL_F_S(Image, SetHeight, int, height, scaleImage)
+STD_SETTER_CMD_IMPL_F_S(Image, SetHeight, int, height, retransform)
 void Image::setHeight(int height) {
 	Q_D(Image);
-	if (height != d->height)
+	if (height != d->height) {
 		exec(new ImageSetHeightCmd(d, height, ki18n("%1: set height")));
+		d->scaleImage();
+	}
 }
 
 STD_SETTER_CMD_IMPL_S(Image, SetKeepRatio, bool, keepRatio)
@@ -279,8 +283,6 @@ void ImagePrivate::scaleImage() {
 
 	if (width != 0 && height != 0)
 		image = image.scaled(width, height, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
-
-	retransform();
 }
 
 void ImagePrivate::updatePosition() {
