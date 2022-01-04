KDE/labplot

commit eb21f0067273fd03ad70d476220fcf2e8da173f2

do a retransform also on elements directly in the worksheet not only the plots

src/backend/core/Project.cpp
@@ -704,25 +704,32 @@ void Project::retransformElements(AbstractAspect* aspect) {
 			curve->recalculate();
 	}
 
-	//all data was read:
-	//call retransform() to every element
-	QVector<CartesianPlot*> plots;
-	if (hasChildren && aspect->type() != AspectType::CartesianPlot)
-		plots = aspect->children<CartesianPlot>(ChildIndexFlag::Recursive);
-	else {
-		if (aspect->type() == AspectType::CartesianPlot)
-			plots << static_cast<CartesianPlot*>(aspect);
-		else if (aspect->inherits(AspectType::XYCurve) || aspect->type() == AspectType::Histogram)
-			plots << static_cast<CartesianPlot*>(aspect->parentAspect());
-	}
-
 	auto childs = aspect->project()->children<WorksheetElement>(ChildIndexFlag::Recursive | ChildIndexFlag::IncludeHidden);
 	for (auto child: childs) {
 		child->setIsLoading(false);
 	}
 
-	for (auto* plot : plots) {
-		plot->retransform(); // retransforms recursive
+	//all data was read:
+	//call retransform() to every element
+	if (hasChildren && aspect->type() != AspectType::CartesianPlot) {
+		auto c = aspect->project()->children<Worksheet>(ChildIndexFlag::IncludeHidden);
+		for (auto child: c) {
+			// retransform all elements in the worksheet (labels, images, plots)
+			// the plots will then recursive retransform the childs of them
+			auto elements = child->children<WorksheetElement>(ChildIndexFlag::IncludeHidden);
+			for (auto e: elements) {
+				e->retransform();
+			}
+		}
+	} else {
+		QVector<CartesianPlot*> plots;
+		if (aspect->type() == AspectType::CartesianPlot)
+			plots << static_cast<CartesianPlot*>(aspect);
+		else if (aspect->inherits(AspectType::XYCurve) || aspect->type() == AspectType::Histogram)
+			plots << static_cast<CartesianPlot*>(aspect->parentAspect());
+
+		for (auto plot: plots)
+			plot->retransform();
 	}
 
 #ifndef SDK
@@ -734,7 +741,7 @@ void Project::retransformElements(AbstractAspect* aspect) {
 	//call CartesianPlot::dataChanged() to notify affected plots about the new data.
 	//this needs to be done here since in LiveDataSource::finalizeImport() called above
 	//where the data is read the column pointers are not restored yes in curves.
-	plots.clear();
+	QVector<CartesianPlot*> plots;
 	for (auto* source : sources) {
 		for (int n = 0; n < source->columnCount(); ++n) {
 			Column* column = source->column(n);

commit 4074e715804a180eb886d6f4fa19ea1882af36fb

Fixing regressions introduced in eb21f0067273fd03ad70d476220fcf2e8da173f2: * recursively retransform all children when loading the project. This fixes projects like "SameStats" having folder structures that were not read. * when copying/duplicating an object, retransform only this object and its children and not all children in the project.

src/backend/core/Project.cpp
@@ -704,20 +704,20 @@ void Project::retransformElements(AbstractAspect* aspect) {
 			curve->recalculate();
 	}
 
-	auto childs = aspect->project()->children<WorksheetElement>(ChildIndexFlag::Recursive | ChildIndexFlag::IncludeHidden);
-	for (auto child: childs) {
+	//set "isLoading" to false for all worksheet elements
+	const auto& children = aspect->children<WorksheetElement>(ChildIndexFlag::Recursive | ChildIndexFlag::IncludeHidden);
+	for (auto* child : children)
 		child->setIsLoading(false);
-	}
 
 	//all data was read:
 	//call retransform() to every element
 	if (hasChildren && aspect->type() != AspectType::CartesianPlot) {
-		auto c = aspect->project()->children<Worksheet>(ChildIndexFlag::IncludeHidden);
-		for (auto child: c) {
+		const auto& worksheets = aspect->children<Worksheet>(ChildIndexFlag::Recursive | ChildIndexFlag::IncludeHidden);
+		for (auto* w : worksheets) {
 			// retransform all elements in the worksheet (labels, images, plots)
 			// the plots will then recursive retransform the childs of them
-			auto elements = child->children<WorksheetElement>(ChildIndexFlag::IncludeHidden);
-			for (auto e: elements) {
+			const auto& elements = w->children<WorksheetElement>(ChildIndexFlag::Recursive | ChildIndexFlag::IncludeHidden);
+			for (auto* e : elements) {
 				if (e->type() == AspectType::CartesianPlot)
 					static_cast<CartesianPlot*>(e)->retransformAll();
 				else
@@ -731,7 +731,7 @@ void Project::retransformElements(AbstractAspect* aspect) {
 		else if (aspect->inherits(AspectType::XYCurve) || aspect->type() == AspectType::Histogram)
 			plots << static_cast<CartesianPlot*>(aspect->parentAspect());
 
-		for (auto plot: plots)
+		for (auto* plot: plots)
 			plot->retransform();
 	}
 