dbf0c2f5e427e22693a4caf7e21475569c7224d4
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2022-01-18 15:33:11 +0100

    text label: fix restoring color when switching back to text mode

--- src/backend/worksheet/TextLabel.cpp
+++ src/backend/worksheet/TextLabel.cpp
@@ -264,25 +264,29 @@ BASIC_SHARED_D_READER_IMPL(TextLabel, qreal, borderOpacity, borderOpacity)
 /* ============================ setter methods and undo commands ================= */
 STD_SETTER_CMD_IMPL_F_S(TextLabel, SetText, TextLabel::TextWrapper, textWrapper, updateText)
 void TextLabel::setText(const TextWrapper &textWrapper) {
-	//DEBUG(Q_FUNC_INFO << ", text = " << STDSTRING(textWrapper.text) << std::endl)
 	Q_D(TextLabel);
+	//DEBUG(Q_FUNC_INFO << ", text = " << STDSTRING(textWrapper.text) << std::endl)
+	//DEBUG(Q_FUNC_INFO << ", old/new mode = " << (int)d->textWrapper.mode << " " << (int)textWrapper.mode)
 
 	if ( (textWrapper.text != d->textWrapper.text) || (textWrapper.mode != d->textWrapper.mode)
 	        || ((d->textWrapper.allowPlaceholder || textWrapper.allowPlaceholder) && (textWrapper.textPlaceholder != d->textWrapper.textPlaceholder)) ||
 			textWrapper.allowPlaceholder != d->textWrapper.allowPlaceholder) {
 		bool changePos = d->textWrapper.text.isEmpty();
 		if (textWrapper.mode == TextLabel::Mode::Text) {
-			//QDEBUG("\n" << Q_FUNC_INFO << ", OLD TEXT = " << d->textWrapper.text <<  ", font color = " << d->fontColor)
+			//QDEBUG("\n" << Q_FUNC_INFO << ", OLD TEXT =" << d->textWrapper.text << ", font color =" << d->fontColor)
 			//DEBUG("\n" << Q_FUNC_INFO << ", NEW TEXT = " << STDSTRING(textWrapper.text) << std::endl)
-			// keep text formatting
-			QTextEdit te(d->textWrapper.text);
-			te.selectAll();
-			te.setText(textWrapper.text);
-			te.selectAll();
-			te.setTextColor(d->fontColor);
 
 			TextWrapper tw = textWrapper;
-			tw.text = te.toHtml();
+			if (d->textWrapper.mode != TextLabel::Mode::Text) {	// restore text formatting
+				QTextEdit te(d->textWrapper.text);
+				te.selectAll();
+				te.setText(textWrapper.text);
+				te.selectAll();
+				te.setTextColor(d->fontColor);
+
+				tw.text = te.toHtml();
+			}
+
 			exec(new TextLabelSetTextCmd(d, tw, ki18n("%1: set label text")));
 		} else
 			exec(new TextLabelSetTextCmd(d, textWrapper, ki18n("%1: set label text")));
