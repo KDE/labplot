e0464dcf937f1ba772973b6fc29a51262a62bb0c
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2022-01-30 15:18:14 +0100

    changelog

--- src/kdefrontend/widgets/LabelWidget.cpp
+++ src/kdefrontend/widgets/LabelWidget.cpp
@@ -4,7 +4,7 @@
     Description          : label settings widget
     --------------------------------------------------------------------
     SPDX-FileCopyrightText: 2008-2022 Alexander Semke <alexander.semke@web.de>
-    SPDX-FileCopyrightText: 2012-2017 Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
+    SPDX-FileCopyrightText: 2012-2022 Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
     SPDX-License-Identifier: GPL-2.0-or-later
 */
 
