4d79e31c043993d0f1ac43643aaf6ac0d61dbb7d
Martin Marmsoler <martin.marmsoler@gmail.com>
2022-02-17 18:35:07 +0100

    retransforming only private is enough, because only datarect should be updated

--- src/backend/worksheet/plots/cartesian/CartesianPlot.cpp
+++ src/backend/worksheet/plots/cartesian/CartesianPlot.cpp
@@ -5083,7 +5083,8 @@ void CartesianPlot::save(QXmlStreamWriter* writer) const {
 }
 
 void CartesianPlot::finalizeLoad() {
-	retransform(); // important to retransform private otherwise datarect needed in retransformScales is incorrect
+	Q_D(CartesianPlot);
+	d->retransform(); // important to retransform private otherwise datarect needed in retransformScales is incorrect
 	retransformScales();
 	retransform(); // important to retransform all childs
 }
