KDE/labplot

commit d4c61765d657b6ad4dfc4db3c082a16e7cdb954d

[spreadsheet] changed the ownership of SpreadsheetModel  -  make Spreadsheet owning it and not SpreadsheetView,  to make sure spreadsheet's children columns are not deleted when the view is closed and the model is deleted.
BUG: 452053
FIXED-IN: 2.9

src/backend/spreadsheet/Spreadsheet.cpp
@@ -73,13 +73,16 @@ void Spreadsheet::init() {
 		addChild(new_col);
 	}
 	setRowCount(rows);
-}
 
+	//the number of columns and rows is known now, initialize the model
+	m_model = new SpreadsheetModel(this);
+}
+/*
 void Spreadsheet::setModel(SpreadsheetModel* model) {
 	m_model = model;
 }
-
-SpreadsheetModel* Spreadsheet::model() {
+*/
+SpreadsheetModel* Spreadsheet::model() const {
 	return m_model;
 }
 
@@ -891,6 +894,9 @@ bool Spreadsheet::load(XmlStreamReader* reader, bool preview) {
 			}
 		}
 	}
+
+	//the number of columns and rows is known now, initialize the model
+	m_model = new SpreadsheetModel(this);
 
 	return !reader->hasError();
 }

src/backend/spreadsheet/Spreadsheet.h
@@ -33,8 +33,7 @@ public:
 	bool printView() override;
 	bool printPreview() const override;
 
-	void setModel(SpreadsheetModel*);
-	SpreadsheetModel* model();
+	SpreadsheetModel* model() const;
 
 	QVector<AspectType> pasteTypes() const override;
 	QVector<AspectType> dropableOn() const override;

src/backend/spreadsheet/SpreadsheetModel.cpp
@@ -53,8 +53,6 @@ SpreadsheetModel::SpreadsheetModel(Spreadsheet* spreadsheet) : QAbstractItemMode
 		beginInsertColumns(QModelIndex(), i, i);
 		handleAspectAdded(spreadsheet->column(i));
 	}
-
-	m_spreadsheet->setModel(this);
 }
 
 void SpreadsheetModel::suppressSignals(bool value) {

src/commonfrontend/spreadsheet/SpreadsheetView.cpp
@@ -97,7 +97,6 @@ enum TukeyLadderPower {InverseSquared, Inverse, InverseSquareRoot, Log, SquareRo
 SpreadsheetView::SpreadsheetView(Spreadsheet* spreadsheet, bool readOnly) : QWidget(),
 	m_tableView(new QTableView(this)),
 	m_spreadsheet(spreadsheet),
-	m_model(new SpreadsheetModel(spreadsheet)),
 	m_readOnly(readOnly) {
 
 	auto* layout = new QVBoxLayout(this);
@@ -130,10 +129,10 @@ SpreadsheetView::SpreadsheetView(Spreadsheet* spreadsheet, bool readOnly) : QWid
 }
 
 SpreadsheetView::~SpreadsheetView() {
-	delete m_model;
 }
 
 void SpreadsheetView::init() {
+	m_model = m_spreadsheet->model();
 	m_tableView->setModel(m_model);
 	auto* delegate = new SpreadsheetItemDelegate(this);
 	connect(delegate, &SpreadsheetItemDelegate::returnPressed, this, &SpreadsheetView::advanceCell);

commit 1c5c7dc069c0dc58010e1b001595378c091cbbd2

[spreadsheet] the fix done in d4c61765d657b6ad4dfc4db3c082a16e7cdb954d has caused test failures on windows and freebsd which are not clear and which don't happen on linux. Reworking the fix trying to adress the test failures.
Changed the logic to create SpreadsheetModel to the previous state where the model was created in the SpreadsheetView and only moved the deletion of it to the desctructor of Spreadsheet.
The model is only needed in the view and it makes sense to create it where and only when it's required. The ownership and the deletion of model is transferred to Spreadsheet - if the view is deleted
or if we want to support multiple views for one Spreadsheet in future, there is no need to create the model again, so we keep it in Spreadsheet.

src/backend/spreadsheet/Spreadsheet.cpp
@@ -57,6 +57,10 @@ Spreadsheet::Spreadsheet(const QString& name, bool loading, AspectType type)
 		init();
 }
 
+Spreadsheet::~Spreadsheet() {
+	delete m_model;
+}
+
 /*!
 	initializes the spreadsheet with the default number of columns and rows
 */
@@ -73,15 +77,12 @@ void Spreadsheet::init() {
 		addChild(new_col);
 	}
 	setRowCount(rows);
-
-	//the number of columns and rows is known now, initialize the model
-	m_model = new SpreadsheetModel(this);
 }
-/*
+
 void Spreadsheet::setModel(SpreadsheetModel* model) {
 	m_model = model;
 }
-*/
+
 SpreadsheetModel* Spreadsheet::model() const {
 	return m_model;
 }
@@ -894,9 +895,6 @@ bool Spreadsheet::load(XmlStreamReader* reader, bool preview) {
 			}
 		}
 	}
-
-	//the number of columns and rows is known now, initialize the model
-	m_model = new SpreadsheetModel(this);
 
 	return !reader->hasError();
 }

src/backend/spreadsheet/Spreadsheet.h
@@ -24,6 +24,7 @@ class Spreadsheet : public AbstractDataSource {
 
 public:
 	explicit Spreadsheet(const QString& name, bool loading = false, AspectType type = AspectType::Spreadsheet);
+	~Spreadsheet() override;
 
 	QIcon icon() const override;
 	QMenu* createContextMenu() override;
@@ -33,6 +34,7 @@ public:
 	bool printView() override;
 	bool printPreview() const override;
 
+	void setModel(SpreadsheetModel*);
 	SpreadsheetModel* model() const;
 
 	QVector<AspectType> pasteTypes() const override;

src/backend/spreadsheet/SpreadsheetModel.cpp
@@ -53,6 +53,8 @@ SpreadsheetModel::SpreadsheetModel(Spreadsheet* spreadsheet) : QAbstractItemMode
 		beginInsertColumns(QModelIndex(), i, i);
 		handleAspectAdded(spreadsheet->column(i));
 	}
+
+	m_spreadsheet->setModel(this);
 }
 
 void SpreadsheetModel::suppressSignals(bool value) {

src/commonfrontend/spreadsheet/SpreadsheetView.cpp
@@ -132,7 +132,14 @@ SpreadsheetView::~SpreadsheetView() {
 }
 
 void SpreadsheetView::init() {
+	//create a new SpreadsheetModel if not available yet.
+	//the creation of the model is done here since it's only required
+	//for the view but its lifecycle is managed in Spreadsheet,
+	//i.e. the deletion of the model is done in the destructor of Spreadsheet
 	m_model = m_spreadsheet->model();
+	if (!m_model)
+		m_model = new SpreadsheetModel(m_spreadsheet);
+
 	m_tableView->setModel(m_model);
 	auto* delegate = new SpreadsheetItemDelegate(this);
 	connect(delegate, &SpreadsheetItemDelegate::returnPressed, this, &SpreadsheetView::advanceCell);