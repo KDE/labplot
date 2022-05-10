9ab35743da26570063a282e04c416743abc15dfe
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2022-05-10 14:55:57 +0200

    code format requires clang-format 13

--- tests/spreadsheet/SpreadsheetTest.cpp
+++ tests/spreadsheet/SpreadsheetTest.cpp
@@ -1082,9 +1082,7 @@ void SpreadsheetTest::testSortPerformanceNumeric1() {
 	col->replaceValues(0, xData);
 
 	// sort
-	QBENCHMARK {
-		sheet.sortColumns(nullptr, {col}, true);
-	}
+	QBENCHMARK { sheet.sortColumns(nullptr, {col}, true); }
 }
 
 /*
@@ -1114,9 +1112,7 @@ void SpreadsheetTest::testSortPerformanceNumeric2() {
 	col1->replaceInteger(0, yData);
 
 	// sort
-	QBENCHMARK {
-		sheet.sortColumns(col0, {col0, col1}, true);
-	}
+	QBENCHMARK { sheet.sortColumns(col0, {col0, col1}, true); }
 }
 
 QTEST_MAIN(SpreadsheetTest)
