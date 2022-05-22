82fa040fd25630904c485f930ff49a5d39cd7b5d
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2022-05-22 18:50:25 +0200

    excel: Fix license header

--- src/backend/datasources/filters/ExcelFilter.cpp
+++ src/backend/datasources/filters/ExcelFilter.cpp
@@ -1,29 +1,13 @@
-/***************************************************************************
-File                 : ExcelFilter.cpp
-Project              : LabPlot
-Description          : Excel I/O-filter
---------------------------------------------------------------------
-Copyright            : (C) 2021 by Fabian Kristof (fkristofszabolcs@gmail.com)
-***************************************************************************/
+/*
+	File                 : ExcelFilter.h
+	Project              : LabPlot
+	Description          : Excel I/O-filter
+	--------------------------------------------------------------------
+	SPDX-FileCopyrightText: 2021 Fabian Kristof (fkristofszabolcs@gmail.com)
+	SPDX-FileCopyrightText: 2022 Stefan Gerlach <stefan.gerlach@uni.kn>
+	SPDX-License-Identifier: GPL-2.0-or-later
+*/
 
-/***************************************************************************
- *                                                                         *
- *  This program is free software; you can redistribute it and/or modify   *
- *  it under the terms of the GNU General Public License as published by   *
- *  the Free Software Foundation; either version 2 of the License, or      *
- *  (at your option) any later version.                                    *
- *                                                                         *
- *  This program is distributed in the hope that it will be useful,        *
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
- *  GNU General Public License for more details.                           *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the Free Software           *
- *   Foundation, Inc., 51 Franklin Street, Fifth Floor,                    *
- *   Boston, MA  02110-1301  USA                                           *
- *                                                                         *
- ***************************************************************************/
 #include "backend/datasources/filters/ExcelFilter.h"
 #include "backend/core/column/Column.h"
 #include "backend/datasources/AbstractDataSource.h"
