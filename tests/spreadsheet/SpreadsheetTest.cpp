21e5c3e96bf02f7c1e70ffb0aef461c559a557b3
Martin Marmsoler <martin.marmsoler@gmail.com>
2022-09-15 19:55:50 +0200

    use already existing script to format. Otherwise the behaviour might be different

--- tests/spreadsheet/SpreadsheetTest.cpp
+++ tests/spreadsheet/SpreadsheetTest.cpp
@@ -1118,7 +1118,9 @@ void SpreadsheetTest::testSortPerformanceNumeric1() {
 	col->replaceValues(0, xData);
 
 	// sort
-	QBENCHMARK { sheet.sortColumns(nullptr, {col}, true); }
+	QBENCHMARK {
+		sheet.sortColumns(nullptr, {col}, true);
+	}
 }
 
 /*
@@ -1148,7 +1150,9 @@ void SpreadsheetTest::testSortPerformanceNumeric2() {
 	col1->replaceInteger(0, yData);
 
 	// sort
-	QBENCHMARK { sheet.sortColumns(col0, {col0, col1}, true); }
+	QBENCHMARK {
+		sheet.sortColumns(col0, {col0, col1}, true);
+	}
 }
 
 QTEST_MAIN(SpreadsheetTest)
