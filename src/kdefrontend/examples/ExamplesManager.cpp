8635fe4591c6d2aee1bf67a08be1c22e0d7ede51
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2022-11-06 10:24:38 +0100

    Fix another QString issue

--- src/kdefrontend/examples/ExamplesManager.cpp
+++ src/kdefrontend/examples/ExamplesManager.cpp
@@ -160,7 +160,7 @@ void ExamplesManager::loadCollections() {
 		QJsonDocument document = QJsonDocument::fromJson(file.readAll());
 		file.close();
 		if (!document.isArray()) {
-			QDEBUG(QStringLiteral("Invalid definition of ") + fileName)
+			QDEBUG("Invalid definition of " << fileName)
 			return;
 		}
 
