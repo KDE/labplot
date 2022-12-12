46808eae89111006b36ad60429eb289166168fa0
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2022-12-12 11:41:47 +0100

    plot designation: don't break compatibility

--- src/backend/core/AbstractColumn.h
+++ src/backend/core/AbstractColumn.h
@@ -34,7 +34,7 @@ class AbstractColumn : public AbstractAspect {
 	Q_ENUMS(ColumnMode)
 
 public:
-	enum class PlotDesignation { NoDesignation, X, Y, Z, XError, XErrorPlus, XErrorMinus, YError, YErrorPlus, YErrorMinus };
+	enum class PlotDesignation { NoDesignation, X, Y, Z, XError, XErrorPlus, XErrorMinus, YError, YErrorMinus, YErrorPlus };
 	enum class ColumnMode {
 		// BASIC FORMATS
 		Double = 0, // double
