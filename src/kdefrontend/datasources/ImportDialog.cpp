KDE/labplot

commit 696b77e5cb6850c48f6ceab9fb0a210ba064227c

[import] properly update header options in ImportFileWidget that are dependent on file type and on the currently selected data container.
Closes #450

src/kdefrontend/datasources/ImportDialog.cpp
@@ -116,7 +116,7 @@ void ImportDialog::setModel() {
 	vLayout->insertWidget(1, frameAddTo);
 
 	connect(tbNewDataContainer, &QToolButton::clicked, this, &ImportDialog::newDataContainerMenu);
-	connect(cbAddTo, &TreeViewComboBox::currentModelIndexChanged, this, &ImportDialog::checkOkButton);
+	connect(cbAddTo, &TreeViewComboBox::currentModelIndexChanged, this, &ImportDialog::currentModelIndexChanged);
 }
 
 void ImportDialog::setCurrentIndex(const QModelIndex& index) {
@@ -124,6 +124,11 @@ void ImportDialog::setCurrentIndex(const QModelIndex& index) {
 	cbAddTo->setCurrentModelIndex(index);
 	QDEBUG("cbAddTo->currentModelIndex() =" << cbAddTo->currentModelIndex());
 	checkOkButton();
+}
+
+void ImportDialog::currentModelIndexChanged(const QModelIndex& index) {
+	auto aspect = static_cast<AbstractAspect*>(index.internalPointer());
+	Q_EMIT dataContainerChanged(aspect);
 }
 
 void ImportDialog::newDataContainer(QAction* action) {

src/kdefrontend/datasources/ImportDialog.h
@@ -58,6 +58,10 @@ protected Q_SLOTS:
 private Q_SLOTS:
 	void newDataContainerMenu();
 	void newDataContainer(QAction*);
+	void currentModelIndexChanged(const QModelIndex&);
+
+Q_SIGNALS:
+	void dataContainerChanged(AbstractAspect*);
 };
 
 #endif // IMPORTDIALOG_H

src/kdefrontend/datasources/ImportFileDialog.cpp
@@ -98,6 +98,7 @@ ImportFileDialog::ImportFileDialog(MainWin* parent, bool liveDataSource, const Q
 	connect(m_importFileWidget, &ImportFileWidget::hostChanged, this, &ImportFileDialog::checkOkButton);
 	connect(m_importFileWidget, &ImportFileWidget::portChanged, this, &ImportFileDialog::checkOkButton);
 	connect(m_importFileWidget, &ImportFileWidget::error, this, &ImportFileDialog::showErrorMessage);
+	connect(this, &ImportDialog::dataContainerChanged, m_importFileWidget, &ImportFileWidget::dataContainerChanged);
 #ifdef HAVE_MQTT
 	connect(m_importFileWidget, &ImportFileWidget::subscriptionsChanged, this, &ImportFileDialog::checkOkButton);
 	connect(m_importFileWidget, &ImportFileWidget::checkFileType, this, &ImportFileDialog::checkOkButton);
@@ -366,12 +367,6 @@ void ImportFileDialog::checkOkButton() {
 		} else {
 			lPosition->setEnabled(true);
 			cbPosition->setEnabled(true);
-
-			// when doing ASCII import to a matrix, hide the options for using the file header (first line)
-			// to name the columns since the column names are fixed in a matrix
-			const auto* matrix = dynamic_cast<const Matrix*>(aspect);
-			m_importFileWidget->showAsciiHeaderOptions(matrix == nullptr);
-			m_importFileWidget->showExcelFirstRowAsColumnOption(matrix == nullptr);
 		}
 	}
 

src/kdefrontend/datasources/ImportFileWidget.cpp
@@ -257,7 +257,7 @@ void ImportFileWidget::loadSettings() {
 	ui.cbSourceType->setCurrentIndex(conf.readEntry("SourceType").toInt());
 
 	// general settings
-	AbstractFileFilter::FileType fileType = static_cast<AbstractFileFilter::FileType>(conf.readEntry("Type", 0));
+	auto fileType = static_cast<AbstractFileFilter::FileType>(conf.readEntry("Type", 0));
 	for (int i = 0; i < ui.cbFileType->count(); ++i) {
 		if (static_cast<AbstractFileFilter::FileType>(ui.cbFileType->itemData(i).toInt()) == fileType) {
 			if (ui.cbFileType->currentIndex() == i)
@@ -444,18 +444,37 @@ void ImportFileWidget::initSlots() {
 #endif
 }
 
-void ImportFileWidget::showAsciiHeaderOptions(bool b) {
-	if (m_asciiOptionsWidget)
-		m_asciiOptionsWidget->showAsciiHeaderOptions(b);
-}
-
-void ImportFileWidget::showExcelFirstRowAsColumnOption(bool show) {
-	ui.lExcelFirstRowAsColNames->setVisible(show);
-	ui.chbExcelFirstRowAsColName->setVisible(show);
+/*!
+ * \brief Called when the current target data containter was changed in ImportDilaog
+ */
+void ImportFileWidget::dataContainerChanged(AbstractAspect* aspect) {
+	m_targetContainer = aspect;
+	updateHeaderOptions();
 }
 
 void ImportFileWidget::enableExcelFirstRowAsColNames(bool enable) {
 	ui.chbExcelFirstRowAsColName->setEnabled(enable);
+}
+
+/*!
+ *  update header specific options that are available for some filter types (ASCII and Excel)
+ *  and for some target data containers (Spreadsheet) only
+ */
+void ImportFileWidget::updateHeaderOptions() {
+	auto fileType = currentFileType();
+	bool spreadsheet = true; // assume it's spreadsheet on default if no container is selected yet
+	if (m_targetContainer)
+		spreadsheet = m_targetContainer->type() == AspectType::Spreadsheet;
+
+	// handle ASCII
+	bool visible = (fileType == AbstractFileFilter::FileType::Ascii) && spreadsheet;
+	if (m_asciiOptionsWidget)
+		m_asciiOptionsWidget->showAsciiHeaderOptions(visible);
+
+	// handle Excel
+	visible = (fileType == AbstractFileFilter::FileType::Excel) && spreadsheet;
+	ui.lExcelFirstRowAsColNames->setVisible(visible);
+	ui.chbExcelFirstRowAsColName->setVisible(visible);
 }
 
 void ImportFileWidget::showJsonModel(bool b) {
@@ -579,8 +598,6 @@ void ImportFileWidget::saveSettings(LiveDataSource* source) const {
 		source->setSerialPort(ui.cbSerialPort->currentText());
 		break;
 	case LiveDataSource::SourceType::MQTT:
-		break;
-	default:
 		break;
 	}
 
@@ -906,7 +923,7 @@ void ImportFileWidget::fileNameChanged(const QString& name) {
 	}
 
 	if (currentSourceType() == LiveDataSource::SourceType::FileOrPipe) {
-		const AbstractFileFilter::FileType fileType = AbstractFileFilter::fileType(fileName);
+		const auto fileType = AbstractFileFilter::fileType(fileName);
 		for (int i = 0; i < ui.cbFileType->count(); ++i) {
 			if (static_cast<AbstractFileFilter::FileType>(ui.cbFileType->itemData(i).toInt()) == fileType) {
 				// automatically select a new file type
@@ -961,7 +978,7 @@ void ImportFileWidget::manageFilters() {
 	and populates the combobox with the available pre-defined filter settings for the selected type.
 */
 void ImportFileWidget::fileTypeChanged(int /*index*/) {
-	AbstractFileFilter::FileType fileType = currentFileType();
+	auto fileType = currentFileType();
 	DEBUG(Q_FUNC_INFO << ", " << ENUM_TO_STRING(AbstractFileFilter, FileType, fileType));
 	initOptionsWidget();
 
@@ -1040,17 +1057,11 @@ void ImportFileWidget::fileTypeChanged(int /*index*/) {
 		ui.lFilter->hide();
 		ui.cbFilter->hide();
 		break;
-	default:
-		DEBUG("unknown file type");
 	}
 
-	if (fileType == AbstractFileFilter::FileType::Excel) {
-		ui.lExcelFirstRowAsColNames->show();
-		ui.chbExcelFirstRowAsColName->show();
-	} else {
-		ui.lExcelFirstRowAsColNames->hide();
-		ui.chbExcelFirstRowAsColName->hide();
-	}
+	// update header specific options that are available for some filter types (ASCII and Excel)
+	// and for some target data containers (Spreadsheet) only
+	updateHeaderOptions();
 
 	int lastUsedFilterIndex = ui.cbFilter->currentIndex();
 	ui.cbFilter->clear();
@@ -1352,7 +1363,7 @@ QString ImportFileWidget::fileInfoString(const QString& name) const {
 */
 void ImportFileWidget::filterChanged(int index) {
 	// ignore filter for these formats
-	AbstractFileFilter::FileType fileType = currentFileType();
+	auto fileType = currentFileType();
 	if (fileType != AbstractFileFilter::FileType::Ascii && fileType != AbstractFileFilter::FileType::Binary) {
 		ui.swOptions->setEnabled(true);
 		return;
@@ -1381,8 +1392,8 @@ void ImportFileWidget::refreshPreview() {
 	WAIT_CURSOR;
 
 	QString file = absolutePath(fileName());
-	AbstractFileFilter::FileType fileType = currentFileType();
-	LiveDataSource::SourceType sourceType = currentSourceType();
+	auto fileType = currentFileType();
+	auto sourceType = currentSourceType();
 	int lines = ui.sbPreviewLines->value();
 
 	if (sourceType == LiveDataSource::SourceType::FileOrPipe)

src/kdefrontend/datasources/ImportFileWidget.h
@@ -4,7 +4,7 @@
 	Description          : import file data widget
 	--------------------------------------------------------------------
 	SPDX-FileCopyrightText: 2009-2017 Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
-	SPDX-FileCopyrightText: 2009-2019 Alexander Semke <alexander.semke@web.de>
+	SPDX-FileCopyrightText: 2009-2022 Alexander Semke <alexander.semke@web.de>
 	SPDX-FileCopyrightText: 2017-2018 Fabian Kristof <fkristofszabolcs@gmail.com>
 	SPDX-FileCopyrightText: 2018-2019 Kovacs Ferencz <kferike98@gmail.com>
 	SPDX-License-Identifier: GPL-2.0-or-later
@@ -68,15 +68,13 @@ public:
 	const QStringList selectedROOTNames() const;
 	const QStringList selectedExcelRegionNames() const;
 
-	void showAsciiHeaderOptions(bool);
-	void showExcelFirstRowAsColumnOption(bool);
-	void showJsonModel(bool);
-	void enableExcelFirstRowAsColNames(bool enable);
-
 	QString host() const;
 	QString port() const;
 	QString serialPort() const;
 	int baudRate() const;
+
+public Q_SLOTS:
+	void dataContainerChanged(AbstractAspect*);
 
 private:
 	Ui::ImportFileWidget ui;
@@ -85,6 +83,9 @@ private:
 	void initOptionsWidget();
 	void initSlots();
 	QString fileInfoString(const QString&) const;
+	void showJsonModel(bool);
+	void enableExcelFirstRowAsColNames(bool enable);
+	void updateHeaderOptions();
 
 	std::unique_ptr<AsciiOptionsWidget> m_asciiOptionsWidget;
 	std::unique_ptr<BinaryOptionsWidget> m_binaryOptionsWidget;
@@ -99,6 +100,7 @@ private:
 
 	mutable std::unique_ptr<AbstractFileFilter> m_currentFilter;
 
+	AbstractAspect* m_targetContainer{nullptr};
 	QTableWidget* m_twPreview{nullptr};
 	KUrlComboBox* m_cbFileName{nullptr};
 	const QString& m_fileName;

commit 83c9eccadbb89679707b28ab16276ba37bfc3325

[import] added missing checkOkButton()-call on data container changes in ImportFileDialog (regression introduced in 696b77e5).

src/kdefrontend/datasources/ImportDialog.cpp
@@ -129,6 +129,7 @@ void ImportDialog::setCurrentIndex(const QModelIndex& index) {
 void ImportDialog::currentModelIndexChanged(const QModelIndex& index) {
 	auto aspect = static_cast<AbstractAspect*>(index.internalPointer());
 	Q_EMIT dataContainerChanged(aspect);
+	checkOkButton();
 }
 
 void ImportDialog::newDataContainer(QAction* action) {