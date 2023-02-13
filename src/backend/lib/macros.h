6a2c4352fe432f36ac416e2ff51c3f04532907cb
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2023-02-13 23:22:08 +0100

    Try fixing STREAM_FORMAT_STATE in macros

--- src/backend/lib/macros.h
+++ src/backend/lib/macros.h
@@ -23,13 +23,13 @@
 // C++ style warning (works on Windows)
 #include <iomanip>
 #include <iostream>
-#define WARN(x) std::cout << std::dec << std::boolalpha << std::setprecision(15) << x << std::endl;
+#define WARN(x) std::cout << std::dec << std::setprecision(15) << std::boolalpha << x << std::endl;
 
 #ifndef NDEBUG
 #include <QDebug>
 #define QDEBUG(x) qDebug() << x;
 // C++ style debugging (works on Windows)
-#define DEBUG(x) std::cout << std::dec << std::boolalpha << std::setprecision(15) << x << std::endl;
+#define DEBUG(x) std::cout << std::dec << std::setprecision(15) << std::boolalpha << x << std::endl;
 #else
 #define QDEBUG(x)                                                                                                                                              \
 	{ }
