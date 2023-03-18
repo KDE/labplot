ea972808784805c973358418f1f42ee4c9bc3167
Martin Marmsoler <martin.marmsoler@gmail.com>
2023-03-18 20:23:47 +0100

    If the html does not contain any background color information, format.background().color() returns black which is not correct

--- src/kdefrontend/widgets/LabelWidget.cpp
+++ src/kdefrontend/widgets/LabelWidget.cpp
@@ -1372,9 +1372,17 @@ void LabelWidget::load() {
 	else
 		ui.kcbFontColor->setColor(m_label->fontColor());
 
-	if (m_label->text().isHtml() && format.background().color().isValid() && !ui.teLabel->toPlainText().isEmpty())
-		ui.kcbBackgroundColor->setColor(format.background().color());
-	else
+	if (m_label->text().isHtml() && format.background().color().isValid() && !ui.teLabel->toPlainText().isEmpty()) {
+		// The html below does not contain any information about the background color. So qt uses black which is not correct
+		// "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n<html><head><meta name=\"qrichtext\"
+		// content=\"1\" /><style type=\"text/css\">\np, li { white-space: pre-wrap; }\n</style></head><body style=\" font-family:'Noto Sans'; font-size:10pt;
+		// font-weight:400; font-style:normal;\">\n<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0;
+		// text-indent:0px;\"><span style=\" color:#000000;\">1</span></p></body></html>"
+		if (m_label->text().text.contains(QStringLiteral("background-color")))
+			ui.kcbBackgroundColor->setColor(format.background().color());
+		else
+			ui.kcbBackgroundColor->setColor(QColor(Qt::GlobalColor::transparent));
+	} else
 		ui.kcbBackgroundColor->setColor(m_label->backgroundColor());
 
 	ui.kfontRequesterTeX->setFont(m_label->teXFont());
