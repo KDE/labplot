a80348e8651cd48ba7d19afda459e9bba2a04a9a
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2023-06-30 23:54:48 +0200

    tests: add xlsx import empty cells

--- tests/import_export/XLSX/ExcelFilterTest.cpp
+++ tests/import_export/XLSX/ExcelFilterTest.cpp
@@ -3,7 +3,7 @@
 	Project              : LabPlot
 	Description          : Tests for the Excel filter
 	--------------------------------------------------------------------
-	SPDX-FileCopyrightText: 2022 Stefan Gerlach <stefan.gerlach@uni.kn>
+	SPDX-FileCopyrightText: 2022-2023 Stefan Gerlach <stefan.gerlach@uni.kn>
 
 	SPDX-License-Identifier: GPL-2.0-or-later
 */
@@ -12,7 +12,7 @@
 #include "backend/datasources/filters/ExcelFilter.h"
 #include "backend/spreadsheet/Spreadsheet.h"
 
-void ExcelFilterTest::importFile1() {
+void ExcelFilterTest::importFile2Cols() {
 	const QString& fileName = QFINDTESTDATA(QLatin1String("data/2col.xlsx"));
 
 	Spreadsheet spreadsheet(QStringLiteral("test"), false);
@@ -37,7 +37,7 @@ void ExcelFilterTest::importFile1() {
 	QCOMPARE(spreadsheet.column(1)->valueAt(4), 1);
 }
 
-void ExcelFilterTest::importFile2() {
+void ExcelFilterTest::importFile3Cols() {
 	const QString& fileName = QFINDTESTDATA(QLatin1String("data/3col.xlsx"));
 
 	Spreadsheet spreadsheet(QStringLiteral("test"), false);
@@ -65,6 +65,44 @@ void ExcelFilterTest::importFile2() {
 	QCOMPARE(spreadsheet.column(2)->valueAt(2), 300);
 	QCOMPARE(spreadsheet.column(2)->valueAt(3), 4000);
 	QCOMPARE(spreadsheet.column(2)->valueAt(4), 50000);
+}
+
+void ExcelFilterTest::importFileEmptyCells() {
+	const QString& fileName = QFINDTESTDATA(QLatin1String("data/datatypes-empty.xlsx"));
+
+	Spreadsheet spreadsheet(QStringLiteral("test"), false);
+	ExcelFilter filter;
+	filter.setCurrentSheet(QStringLiteral("Sheet1"));
+	filter.setCurrentRange(QStringLiteral("A1:F5"));
+	filter.readDataFromFile(fileName, &spreadsheet);
+
+	QCOMPARE(spreadsheet.columnCount(), 6);
+	QCOMPARE(spreadsheet.rowCount(), 5);
+
+	WARN(spreadsheet.column(0)->valueAt(0))
+	WARN(spreadsheet.column(0)->valueAt(1))
+	WARN(spreadsheet.column(0)->valueAt(2))
+	WARN(spreadsheet.column(0)->valueAt(3))
+	WARN(spreadsheet.column(0)->valueAt(4))
+	WARN(spreadsheet.column(1)->valueAt(0))
+	WARN(spreadsheet.column(2)->valueAt(0))
+	WARN(spreadsheet.column(3)->valueAt(0))
+	WARN(spreadsheet.column(4)->valueAt(0))
+	WARN(spreadsheet.column(5)->valueAt(0))
+	//	QCOMPARE(spreadsheet.column(0)->valueAt(0), 45107.7371295949);
+	QCOMPARE(spreadsheet.column(0)->valueAt(1), 10.5);
+	//	QCOMPARE(spreadsheet.column(0)->valueAt(2), 43747);
+	//	QCOMPARE(spreadsheet.column(0)->valueAt(3), 0.422974537037037);
+	QCOMPARE(spreadsheet.column(0)->valueAt(4), 40000);
+	//	QCOMPARE(spreadsheet.column(1)->valueAt(0), 45107.7371180556);
+	QCOMPARE(spreadsheet.column(2)->valueAt(0), 10.5);
+	//	QCOMPARE(spreadsheet.column(3)->valueAt(0), 43747);
+	//	QCOMPARE(spreadsheet.column(4)->valueAt(0), 0.422974537037037);
+	QCOMPARE(spreadsheet.column(5)->valueAt(0), 40000);
+
+	for (int col = 1; col < 6; col++)
+		for (int row = 1; row < 5; row++)
+			QCOMPARE(spreadsheet.column(col)->valueAt(row), 0);
 }
 
 QTEST_MAIN(ExcelFilterTest)
