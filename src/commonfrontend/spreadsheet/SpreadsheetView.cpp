e2be5c2d4ce4ebf326033ec798e09f6ae5435e98
Alexander Semke <alexander.semke@web.de>
2023-07-08 11:02:46 +0200

    [spreadsheet] react of Delete and Backspace keys to delete cells in spreadsheet columns.
    fix #596

--- src/commonfrontend/spreadsheet/SpreadsheetView.cpp
+++ src/commonfrontend/spreadsheet/SpreadsheetView.cpp
@@ -3350,7 +3350,8 @@ void SpreadsheetView::clearSelectedCells() {
 
 	// don't try to clear values if the selected cells don't have any values at all
 	bool empty = true;
-	for (auto* column : selectedColumns()) {
+	const auto& columns = selectedColumns(false);
+	for (auto* column : columns) {
 		for (int row = last; row >= first; row--) {
 			if (column->isValid(row)) {
 				empty = false;
@@ -3366,7 +3367,7 @@ void SpreadsheetView::clearSelectedCells() {
 
 	WAIT_CURSOR;
 	m_spreadsheet->beginMacro(i18n("%1: clear selected cells", m_spreadsheet->name()));
-	for (auto* column : selectedColumns()) {
+	for (auto* column : columns) {
 		column->setSuppressDataChangedSignal(true);
 		// 		if (formulaModeActive()) {
 		// 			int col = m_spreadsheet->indexOfChild<Column>(column);
