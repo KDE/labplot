e64a23970355f3e43818dfc20a9e42e25442a786
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2023-07-24 01:20:43 +0200

    clean up

--- src/backend/nsl/nsl_baseline.cpp
+++ src/backend/nsl/nsl_baseline.cpp
@@ -131,60 +131,60 @@ double nsl_baseline_remove_arpls_Eigen3(double* data, const size_t n, double p, 
 	typedef Eigen::SparseMatrix<double> SMat; // declares a column-major sparse matrix type of double
 	typedef Eigen::SparseVector<double> SVec; // declares a sparse vector type of double
 
-	Eigen::SparseMatrix<double> Dmat(n, n - 2);
+	Eigen::SparseMatrix<double> D(n, n - 2);
 	for (size_t i = 0; i < n; ++i) {
 		for (size_t j = 0; j < n - 2; ++j) {
 			if (i == j)
-				Dmat.insert(i, j) = 1.;
+				D.insert(i, j) = 1.;
 			if (i == j + 1)
-				Dmat.insert(i, j) = -2.;
+				D.insert(i, j) = -2.;
 			if (i == j + 2)
-				Dmat.insert(i, j) = 1.;
+				D.insert(i, j) = 1.;
 		}
 	}
-	// std::cout << "Dmat =" << std::endl << Dmat << std::endl;
+	// std::cout << "D =" << std::endl << D << std::endl;
 
 	// H = lambda * D.dot(D.T)
-	SMat Hmat = lambda * Dmat * Dmat.transpose();
-	// std::cout << "Hmat =" << std::endl << Hmat << std::endl;
+	SMat H = lambda * D * D.transpose();
+	// std::cout << "H =" << std::endl << H << std::endl;
 
 	// weights
-	SVec wvec(n);
-	SMat Wmat(n, n);
+	SVec w(n);
+	SMat W(n, n);
 	for (size_t i = 0; i < n; ++i) {
-		wvec.insert(i) = 1.;
-		Wmat.insert(i, i) = 1.;
+		w.insert(i) = 1.;
+		W.insert(i, i) = 1.;
 	}
 
-	SVec dvec(n); // data
+	SVec d(n); // data
 	for (size_t i = 0; i < n; i++)
-		dvec.insert(i) = data[i];
-	SVec zvec(n); // solution
+		d.insert(i) = data[i];
+	SVec z(n); // solution
 	for (size_t i = 0; i < n; i++) // initial guess for z
-		zvec.insert(i) = data[i];
+		z.insert(i) = data[i];
 
 	while (crit > p) {
 		printf("iteration %d\n", count);
 
 		// solve (W+H)z = W*data
 		Eigen::SimplicialLLT<SMat> solver;
-		solver.compute(Wmat + Hmat);
+		solver.compute(W + H);
 		if (solver.info() != Eigen::Success)
 			puts("decomposition failed\n");
 
-		zvec = solver.solve(Wmat * dvec);
+		z = solver.solve(W * d);
 		if (solver.info() != Eigen::Success)
 			puts("solving failed\n");
 
-		// std::cout << "zvec = " << zvec << std::endl;
+		// std::cout << "z = " << z << std::endl;
 
-		SVec diffvec = dvec - zvec;
-		// std::cout << "diffvec = " << diffvec << std::endl;
+		SVec diff = d - z;
+		// std::cout << "diff = " << diff << std::endl;
 
 		// mean and stdev of negative diffs
 		double m = 0.;
 		int num = 0;
-		for (SVec::InnerIterator it(diffvec); it; ++it) {
+		for (SVec::InnerIterator it(diff); it; ++it) {
 			double v = it.value();
 			if (v < 0) {
 				m += v;
@@ -195,7 +195,7 @@ double nsl_baseline_remove_arpls_Eigen3(double* data, const size_t n, double p, 
 			m /= num;
 		// printf("m = %g\n", m);
 		double s = 0.;
-		for (SVec::InnerIterator it(diffvec); it; ++it) {
+		for (SVec::InnerIterator it(diff); it; ++it) {
 			double v = it.value();
 			if (v < 0)
 				s += (v - m) * (v - m);
@@ -206,20 +206,19 @@ double nsl_baseline_remove_arpls_Eigen3(double* data, const size_t n, double p, 
 		// printf("s = %g\n", s);
 
 		// w_new = 1 / (1 + np.exp(2 * (d - (2*s - m))/s))
-		SVec wnvec(n);
+		SVec wn(n);
 		for (size_t i = 0; i < n; ++i)
-			wnvec.insert(i) = 1. / (1. + exp(2. * (diffvec.coeffRef(i) - (2. * s - m)) / s));
+			wn.insert(i) = 1. / (1. + exp(2. * (diff.coeffRef(i) - (2. * s - m)) / s));
 		// std::cout << "wnvec = " << wnvec << std::endl;
 
 		// crit = norm(w_new - w) / norm(w)
-		crit = (wnvec - wvec).norm() / wvec.norm();
+		crit = (wn - w).norm() / w.norm();
 		printf("crit = %.15g\n", crit);
 
-		// w = w_new
-		wvec = wnvec;
+		w = wn;
 		// W.setdiag(w)
 		for (size_t i = 0; i < n; ++i)
-			Wmat.coeffRef(i, i) = wnvec.coeffRef(i);
+			W.coeffRef(i, i) = wn.coeffRef(i);
 		// std::cout << "Wmat = " << Wmat << std::endl;
 
 		count++;
@@ -231,7 +230,7 @@ double nsl_baseline_remove_arpls_Eigen3(double* data, const size_t n, double p, 
 	}
 
 	for (size_t i = 0; i < n; ++i)
-		data[i] -= zvec.coeffRef(i);
+		data[i] -= z.coeffRef(i);
 #endif
 
 	return crit;
