7adb8dc28c42af3f849dc403e808859c7c21e37c
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2024-01-11 23:47:27 +0100

    tests: split up axis test to avoid occasional timeouts

--- src/backend/worksheet/plots/cartesian/Axis.h
+++ src/backend/worksheet/plots/cartesian/Axis.h
@@ -291,6 +291,7 @@ Q_SIGNALS:
 
 	friend class RetransformTest;
 	friend class AxisTest;
+	friend class AxisTest2;
 };
 
 Q_DECLARE_OPERATORS_FOR_FLAGS(Axis::TicksDirection)
--- tests/backend/Axis/AxisTest.h
+++ tests/backend/Axis/AxisTest.h
@@ -1,7 +1,7 @@
 /*
-	File                 : WorksheetElementTest.h
+	File                 : AxisTest.h
 	Project              : LabPlot
-	Description          : Tests for WorksheetElements in the graphical representation sense
+	Description          : Tests for Axis methods
 	--------------------------------------------------------------------
 	SPDX-FileCopyrightText: 2022 Martin Marmsoler <martin.marmsoler@gmail.com>
 
@@ -41,26 +41,6 @@ private Q_SLOTS:
 	void columnLabelValuesMaxValues();
 
 	void customTextLabels();
-
-	void dateTime();
-	void dateTimeSpacing();
-	void dateTimeSpacingOffsetNonZero();
-	void dateTimeSpacingStartValueNonZero();
-
-	void numeric();
-	void numericSpacing();
-	void numericSpacingOffsetNonZero();
-	void numericSpacingStartValueNonZero();
-
-	void customColumnNumeric();
-	void customColumnNumericMaxValues();
-	void customColumnNumericMaxValuesLimitedRange();
-	void customColumnNumericMaxValuesLimitedRangeNotCompleteRange();
-	void customColumnNonMonotonicColumnValues();
-	void customColumnDateTime();
-
-	void autoScale();
-	void autoScale2();
 };
 
 #endif // AXISTEST_H
