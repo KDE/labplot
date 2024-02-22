e7b4a463cf20a7cdb2b787d182d57a9f80ef5fdb
Alexander Semke <alexander.semke@web.de>
2024-02-22 19:48:39 +0100

    [spreadsheet] minor fixes and improvements: * properly shown the action name "hide/show" for comments and sparklines when being toggled from different context menus * don't shown the "toggle comments/sparklines" actions in the column context menu, those are for the whole spreadsheet * fixed layout in the spreadsheet dock * removed redundant code in the generation of the sparkline pixmaps

--- src/commonfrontend/spreadsheet/SparklineRunnable.cpp
+++ src/commonfrontend/spreadsheet/SparklineRunnable.cpp
@@ -5,14 +5,13 @@
 	SPDX-FileCopyrightText: 2023 Kuntal Bar <barkuntal6@gmail.com>
 	SPDX-License-Identifier: GPL-2.0-or-later
 */
+
 #include "SparklineRunnable.h"
-#include "backend/lib/trace.h"
-#include "backend/worksheet/Line.h"
 #include "backend/worksheet/Worksheet.h"
-#include "backend/worksheet/plots/PlotArea.h"
 #include "backend/worksheet/plots/cartesian/BarPlot.h"
 #include "backend/worksheet/plots/cartesian/XYCurve.h"
-#include "qwidget.h"
+
+#include <QWidget>
 
 void SparkLineRunnable::run() {
 	if (col->columnMode() != Column::ColumnMode::Text && !col->isPlottable()) {
@@ -20,30 +19,30 @@ void SparkLineRunnable::run() {
 		mPixmap.fill(QColor(49, 54, 59));
 		return;
 	}
+
 	static const QString sparklineTheme = QStringLiteral("Sparkline");
 	static const QString sparklineText = QStringLiteral("add-sparkline");
+
 	auto* worksheet = new Worksheet(sparklineText);
-	worksheet->setTheme(sparklineTheme);
+	worksheet->setUndoAware(false);
 	worksheet->setUseViewSize(true);
 	worksheet->setLayoutBottomMargin(0);
 	worksheet->setLayoutTopMargin(0);
 	worksheet->setLayoutLeftMargin(0);
 	worksheet->setLayoutRightMargin(0);
-	auto* plot = new CartesianPlot(sparklineText);
-	plot->setTheme(sparklineTheme);
-	plot->setSuppressRetransform(true);
-	plot->setType(CartesianPlot::Type::TwoAxes);
-	worksheet->addChild(plot);
-	plot->setSuppressRetransform(false);
 
+	auto* plot = new CartesianPlot(sparklineText);
+	plot->setSuppressRetransform(true);
+	plot->setVerticalPadding(2);
+	plot->setRightPadding(2);
+	plot->setBottomPadding(2);
+	plot->setHorizontalPadding(2);
+	worksheet->addChild(plot);
+
+	QApplication::processEvents(QEventLoop::AllEvents, 100);
 	if (col->columnMode() == Column::ColumnMode::Text) {
 		auto* barPlot = new BarPlot(QString());
-		plot->addChild(barPlot);
-		plot->setVerticalPadding(0);
-		plot->setRightPadding(0);
-		plot->setBottomPadding(0);
-		plot->setHorizontalPadding(0);
-
+		barPlot->setSuppressRetransform(true);
 		barPlot->setOrientation(BarPlot::Orientation::Vertical);
 
 		// generate columns holding the data and the labels
@@ -63,12 +62,11 @@ void SparkLineRunnable::run() {
 		for (const auto& pair : qAsConst(pairs))
 			data << pair.second;
 		dataColumn->replaceInteger(0, data);
-		QVector<const AbstractColumn*> columns;
-		columns << dataColumn;
+		QVector<const AbstractColumn*> columns{dataColumn};
 		barPlot->setDataColumns(columns);
-
+		plot->addChild(barPlot);
+		barPlot->setSuppressRetransform(false);
 	} else {
-		QApplication::processEvents(QEventLoop::AllEvents, 100);
 		const int rowCount = col->rowCount();
 		QVector<int> xData(rowCount);
 		xData.resize(rowCount);
@@ -83,18 +81,18 @@ void SparkLineRunnable::run() {
 		curve->setSuppressRetransform(false);
 		curve->setXColumn(xColumn);
 		curve->setYColumn(col);
-		curve->setSuppressRetransform(false);
 		plot->addChild(curve);
-		plot->setVerticalPadding(0);
-		plot->setRightPadding(0);
-		plot->setBottomPadding(0);
-		plot->setHorizontalPadding(0);
+		curve->setSuppressRetransform(false);
 	}
-	worksheet->setSuppressLayoutUpdate(false);
-	worksheet->updateLayout();
+
+	worksheet->setTheme(sparklineTheme);
+	plot->setSuppressRetransform(false);
+	plot->retransform();
+
 	// Export to pixmap
 	mPixmap = QPixmap(worksheet->view()->size());
 	worksheet->exportView(mPixmap);
+
 	delete worksheet;
 	Q_EMIT taskFinished(mPixmap);
 }
