fd613dda3a01c037162b14cdd67ac0a72ade08f3
Alexander Semke <alexander.semke@web.de>
2024-05-10 20:49:14 +0200

    [worksheet] disallow word wrap in the text label.
    Previously QGraphictTextItem::adjustSize() was added in TextLabelPrivate::updateText()
    to update the recalculationg of the text alignment (left align, etc.). This call
    triggers word wrap for some reasons. To avoid this, explicitly switch off the word wrap
    via the text options.
    
    fix #895

--- src/backend/worksheet/TextLabel.cpp
+++ src/backend/worksheet/TextLabel.cpp
@@ -50,6 +50,10 @@ class ScaledTextItem : public QGraphicsTextItem {
 public:
 	explicit ScaledTextItem(QGraphicsItem* parent = nullptr)
 		: QGraphicsTextItem(parent) {
+
+		auto textOptions = document()->defaultTextOption();
+		textOptions.setWrapMode(QTextOption::NoWrap);
+		document()->setDefaultTextOption(textOptions);
 	}
 
 	QRectF scaledBoundingRect() const {
