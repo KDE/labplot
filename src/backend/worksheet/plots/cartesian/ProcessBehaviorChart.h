c39a39392e85f943669ff1783e2c22c699f0d972
Martin Marmsoler <martin.marmsoler@gmail.com>
2024-12-21 13:01:10 +0100

    implement indicesMinMax for every plot type
    Reason: So the cartesianPlot gets much simpler

--- src/backend/worksheet/plots/cartesian/ProcessBehaviorChart.h
+++ src/backend/worksheet/plots/cartesian/ProcessBehaviorChart.h
@@ -69,6 +69,7 @@ public:
 	void setVisible(bool) override;
 
 	int xIndexCount() const;
+	bool indicesMinMax(const Dimension dim, double v1, double v2, int& start, int& end) const override;
 	bool minMax(const CartesianCoordinateSystem::Dimension dim, const Range<int>& indexRange, Range<double>& r, bool includeErrorBars = true) const override;
 	double minimum(CartesianCoordinateSystem::Dimension) const override;
 	double maximum(CartesianCoordinateSystem::Dimension) const override;
