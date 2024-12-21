2d952347418630ff60eef5d9524997d600b81158
Martin Marmsoler <martin.marmsoler@gmail.com>
2024-12-21 12:02:00 +0100

    Fix test
    Files: Project.cpp

--- src/backend/core/Project.cpp
+++ src/backend/core/Project.cpp
@@ -1053,7 +1053,7 @@ void Project::restorePointers(AbstractAspect* aspect) {
 	QVector<Heatmap*> heatmaps;
 	if (hasChildren)
 		heatmaps = aspect->children<Heatmap>(ChildIndexFlag::Recursive);
-	else if (aspect->type() == AspectType::Histogram)
+	else if (aspect->type() == AspectType::Heatmap)
 		heatmaps << static_cast<Heatmap*>(aspect);
 
 	for (auto* heatmap : heatmaps) {
