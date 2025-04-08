1621f074da599e446b3bf040d2dd888e037c7351
Kuntal Bar <barkuntal6@gmail.com>
2025-04-09 02:40:50 +0530

    fix format

--- src/backend/worksheet/ResizeItem.cpp
+++ src/backend/worksheet/ResizeItem.cpp
@@ -185,10 +185,10 @@ WorksheetElementContainer* ResizeItem::container() {
 void ResizeItem::paint(QPainter*, const QStyleOptionGraphicsItem*, QWidget*) {
 }
 
-#define IMPL_SET_FN(TYPE, POS)                                                                                                                                \
-	void ResizeItem::set##POS(TYPE v) {                                                                                                                      \
-		m_rect.set##POS(v);                                                                                                                                  \
-		m_container->setRect(mapRectToScene(m_rect));                                                                                                        \
+#define IMPL_SET_FN(TYPE, POS)                                                                                                                                 \
+	void ResizeItem::set##POS(TYPE v) {                                                                                                                        \
+		m_rect.set##POS(v);                                                                                                                                    \
+		m_container->setRect(mapRectToScene(m_rect));                                                                                                          \
 	}
 
 IMPL_SET_FN(qreal, Top)
