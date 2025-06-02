f43e4fe46f03d6921f04c30eba7d5f9ffe9664e2
Alexander Semke <alexander.semke@web.de>
2025-06-02 19:58:50 +0200

    [statistics] removed the datasource option and more cleanup also in the dock class files.

--- src/frontend/dockwidgets/HypothesisTestDock.cpp
+++ src/frontend/dockwidgets/HypothesisTestDock.cpp
@@ -1,24 +1,20 @@
 /*
 	File                 : HypothesisTestDock.cpp
 	Project              : LabPlot
-	Description          : Dock for One-Sample T-Test
+	Description          : Dock for Hypothesis Tests
 	--------------------------------------------------------------------
-	SPDX-FileCopyrightText: 2019  Devanshu Agarwal (agarwaldevanshu8@gmail.com)
-	SPDX-FileCopyrightText: 2023 Alexander Semke <alexander.semke@web.de>
+	SPDX-FileCopyrightText: 2019  Devanshu Agarwal <agarwaldevanshu8@gmail.com>
+	SPDX-FileCopyrightText: 2023-205 Alexander Semke <alexander.semke@web.de>
 	SPDX-FileCopyrightText: 2025 Kuntal Bar <barkuntal6@gmail.com>
 
- SPDX-License-Identifier: GPL-2.0-or-later
+	SPDX-License-Identifier: GPL-2.0-or-later
 */
 
 #include "HypothesisTestDock.h"
 #include "TreeViewComboBox.h"
-#include "backend/core/AspectTreeModel.h"
-#include "backend/core/Project.h"
 #include "backend/spreadsheet/Spreadsheet.h"
 #include "frontend/widgets/TreeViewComboBox.h"
 
-#include <QDoubleValidator>
-#include <QIcon>
 #include <QLocale>
 #include <QPushButton>
 
@@ -32,13 +28,18 @@ HypothesisTestDock::HypothesisTestDock(QWidget* parent)
 	ui.setupUi(this);
 	setBaseWidgets(ui.leName, ui.teComment);
 
-	spreadsheetComboBox = new TreeViewComboBox;
-	ui.gridLayout->addWidget(spreadsheetComboBox, 4, 3, 1, 3);
+	// combo boxes for sample columns
+	cbColumn1 = new TreeViewComboBox(this);
+	ui.gridLayout->addWidget(cbColumn1, 8, 3, 1, 3);
+	cbColumn2 = new TreeViewComboBox(this);
+	ui.gridLayout->addWidget(cbColumn2, 9, 3, 1, 3);
+	cbColumn3 = new TreeViewComboBox(this);
+	ui.gridLayout->addWidget(cbColumn3, 10, 3, 1, 3);
 
-	ui.cbTest->addItem(i18n("One-Sample T-Test"));
-
-	ui.lPopulationSigma->setText(UTF8_QSTRING("σ"));
-	ui.chbCalculateStats->setChecked(true);
+	QList<AspectType> list{AspectType::Folder, AspectType::Workbook, AspectType::Spreadsheet, AspectType::Notebook, AspectType::Column};
+	cbColumn1->setTopLevelClasses(list);
+	cbColumn2->setTopLevelClasses(list);
+	cbColumn3->setTopLevelClasses(list);
 
 	ui.pbLeveneTest->hide();
 	ui.lCategorical->hide();
@@ -53,81 +54,86 @@ HypothesisTestDock::HypothesisTestDock(QWidget* parent)
 
 	const QString mu = UTF8_QSTRING("μ");
 	const QString mu0 = UTF8_QSTRING("μₒ");
-
+	ui.lPopulationSigma->setText(UTF8_QSTRING("σ"));
 	ui.rbH1OneTail1->setText(i18n("%1 > %2", mu, mu0));
 	ui.rbH1OneTail2->setText(i18n("%1 < %2", mu, mu0));
 	ui.rbH1TwoTail->setText(i18n("%1 ≠ %2", mu, mu0));
 	ui.rbH0OneTail1->setText(i18n("%1 ≤ %2", mu, mu0));
 	ui.rbH0OneTail2->setText(i18n("%1 ≥ %2", mu, mu0));
 	ui.rbH0TwoTail->setText(i18n("%1 = %2", mu, mu0));
-
 	ui.lMuo->setText(QStringLiteral("μ₀"));
 	ui.lAlpha->setText(QString::fromUtf8("α"));
+
 	ui.leMuo->setText(QLocale().toString(0.0));
 	ui.leAlpha->setText(QLocale().toString(0.05));
 	ui.leMuo->setValidator(new QDoubleValidator(ui.leMuo));
 	ui.leAlpha->setValidator(new QDoubleValidator(ui.leAlpha));
+
 	ui.pbPerformTest->setIcon(QIcon::fromTheme(QLatin1String("run-build")));
 
 	ui.rbH1TwoTail->setEnabled(false);
 	ui.rbH1OneTail1->setEnabled(false);
 	ui.rbH1OneTail2->setEnabled(false);
 
+	retranslateUi();
+
+	//**********************************  Slots **********************************************
+	// connect(cbColumn1, &TreeViewComboBox::currentModelIndexChanged, this, &HypothesisTestDock::xColumnChanged);
+	connect(ui.cbTest, static_cast<void (QComboBox::*)(int)>(&QComboBox::currentIndexChanged), this, &HypothesisTestDock::testChanged);
 	connect(ui.rbH0TwoTail, &QRadioButton::clicked, this, &HypothesisTestDock::onNullTwoTailClicked);
 	connect(ui.rbH0OneTail1, &QRadioButton::clicked, this, &HypothesisTestDock::onNullOneTail1Clicked);
 	connect(ui.rbH0OneTail2, &QRadioButton::clicked, this, &HypothesisTestDock::onNullOneTail2Clicked);
-
-	connect(spreadsheetComboBox, &TreeViewComboBox::currentModelIndexChanged, this, &HypothesisTestDock::onSpreadsheetSelectionChanged);
 	connect(ui.pbPerformTest, &QPushButton::clicked, this, &HypothesisTestDock::runHypothesisTest);
-
-	ui.cbTest->setCurrentIndex(0);
-	Q_EMIT ui.cbTest->currentIndexChanged(0);
 }
 
-void HypothesisTestDock::initializeTest(HypothesisTest* test) {
+void HypothesisTestDock::setTest(HypothesisTest* test) {
 	CONDITIONAL_LOCK_RETURN
 	m_test = test;
+	setAspects(QList<AbstractAspect*>{m_test});
 
-	m_aspectTreeModel = new AspectTreeModel(m_test->project());
-
-	const QList<AspectType> topLevelAspects{AspectType::Folder, AspectType::Workbook, AspectType::Spreadsheet, AspectType::LiveDataSource};
-	spreadsheetComboBox->setTopLevelClasses(topLevelAspects);
-
-	const QList<AspectType> selectableAspects{AspectType::Spreadsheet, AspectType::LiveDataSource};
-	m_aspectTreeModel->setSelectableAspects(selectableAspects);
-	spreadsheetComboBox->setModel(m_aspectTreeModel);
-
-	ui.leName->setText(m_test->name());
-	ui.teComment->setText(m_test->comment());
-	updateAspectComboBoxIndex(spreadsheetComboBox, m_test->getDataSourceSpreadsheet());
-	refreshColumnComboBox(m_test->getDataSourceSpreadsheet());
-
-	connect(m_test, &HypothesisTest::aspectDescriptionChanged, this, &HypothesisTestDock::aspectDescriptionChanged);
+	QList<AspectType> list{AspectType::Column};
+	auto* model = aspectModel();
+	model->setSelectableAspects(list);
+	cbColumn1->setModel(model);
+	cbColumn2->setModel(model);
+	cbColumn3->setModel(model);
 }
 
 void HypothesisTestDock::retranslateUi() {
 	ui.retranslateUi(this);
+
 	ui.cbTest->clear();
-	ui.cbTest->addItem(i18n("One-Sample T-Test"));
-	ui.lPopulationSigma->setText(UTF8_QSTRING("σ"));
-	const QString mu = UTF8_QSTRING("μ");
-	const QString mu0 = UTF8_QSTRING("μₒ");
-	ui.rbH1OneTail1->setText(i18n("%1 > %2", mu, mu0));
-	ui.rbH1OneTail2->setText(i18n("%1 < %2", mu, mu0));
-	ui.rbH1TwoTail->setText(i18n("%1 ≠ %2", mu, mu0));
-	ui.rbH0OneTail1->setText(i18n("%1 ≤ %2", mu, mu0));
-	ui.rbH0OneTail2->setText(i18n("%1 ≥ %2", mu, mu0));
-	ui.rbH0TwoTail->setText(i18n("%1 = %2", mu, mu0));
-	ui.lMuo->setText(QStringLiteral("μ₀"));
-	ui.lAlpha->setText(QString::fromUtf8("α"));
-	ui.pbPerformTest->setIcon(QIcon::fromTheme(QLatin1String("run-build")));
+	ui.cbTest->addItem(i18n("One-Sample t-Test"), static_cast<int>(HypothesisTest::Test::t_test_one_sample));
+	ui.cbTest->addItem(i18n("Two-Sample t-Test (Independent)"), static_cast<int>(HypothesisTest::Test::t_test_two_sample));
+	ui.cbTest->addItem(i18n("Paired t-Test"), static_cast<int>(HypothesisTest::Test::t_test_one_sample));
+	ui.cbTest->addItem(i18n("One-Way ANOVA"), static_cast<int>(HypothesisTest::Test::t_test_one_sample));
+	ui.cbTest->addItem(i18n("Mann-Whitney U Test"), static_cast<int>(HypothesisTest::Test::t_test_one_sample));
+	ui.cbTest->addItem(i18n("Kruskal-Wallis Test"), static_cast<int>(HypothesisTest::Test::t_test_one_sample));
+	ui.cbTest->addItem(i18n("Logrank Test"), static_cast<int>(HypothesisTest::Test::t_test_one_sample));
+
+	// tooltip texts
+	QString info = i18n(
+		"<ul>"
+		"<li><b>One-Sample t-Test</b> - tests whether the mean of a single sample differs significantly from a known or hypothesized population mean</li>"
+		"<li><b>Two-Sample t-Test (Independent)</b> - compares the means of two independent groups to determine if they are significantly different</li>"
+		"<li><b>Paired t-Test</b> - tests the difference between paired observations (e.g., before and after measurements) to see if the mean difference is significantly different from zero</li>"
+		"<li><b>One-Way ANOVA</b> - tests whether there are significant differences among the means of three or more independent groups</li>"
+		"<li><b>Mann-Whitney U Test</b> - </li>"
+		"<li><b>Kruskal-Wallis Test</b> - </li>"
+		"<li><b>Logrank Test</b> - </li>"
+		"</ul>"
+	);
+	ui.lTest->setToolTip(info);
+	ui.cbTest->setToolTip(info);
 }
 
-void HypothesisTestDock::updateAspectComboBoxIndex(TreeViewComboBox* comboBox, const AbstractAspect* aspect) {
-	if (aspect)
-		comboBox->setCurrentModelIndex(m_aspectTreeModel->modelIndexOfAspect(aspect));
-	else
-		comboBox->setCurrentModelIndex(QModelIndex());
+// ##############################################################################
+// ####################################  SLOTs   ################################
+// ##############################################################################
+void HypothesisTestDock::testChanged(int) {
+	auto test = static_cast<HypothesisTest::Test>(ui.cbTest->currentData().toInt());
+
+	// TODO.:
 }
 
 void HypothesisTestDock::onNullTwoTailClicked() {
@@ -174,31 +180,11 @@ void HypothesisTestDock::runHypothesisTest() {
 		return; // TODO: Handle conversion error
 	m_test->setSignificanceLevel(alpha);
 
-	if (ui.cbCol1->count() == 0)
-		return;
-	QVector<Column*> columns;
-	columns << reinterpret_cast<Column*>(ui.cbCol1->currentData().toLongLong());
-	m_test->setColumns(columns);
+	// if (ui.cbCol1->count() == 0)
+	// 	return;
+	// QVector<Column*> columns;
+	// columns << reinterpret_cast<Column*>(ui.cbCol1->currentData().toLongLong());
+	// m_test->setColumns(columns);
 
 	m_test->runTest();
-}
-
-void HypothesisTestDock::onSpreadsheetSelectionChanged(const QModelIndex& index) {
-	auto* aspect = static_cast<AbstractAspect*>(index.internalPointer());
-	auto* spreadsheet = dynamic_cast<Spreadsheet*>(aspect);
-	refreshColumnComboBox(spreadsheet);
-	m_test->setDataSourceSpreadsheet(spreadsheet);
-}
-
-void HypothesisTestDock::refreshColumnComboBox(Spreadsheet* spreadsheet) {
-	ui.cbCol1->clear();
-	for (auto* col : spreadsheet->children<Column>()) {
-		if (col->columnMode() == AbstractColumn::ColumnMode::Integer || col->columnMode() == AbstractColumn::ColumnMode::Double)
-			ui.cbCol1->addItem(col->name(), qint64(col));
-	}
-	ui.pbPerformTest->setEnabled(hasSelectedColumns());
-}
-
-bool HypothesisTestDock::hasSelectedColumns() const {
-	return ui.cbCol1->isVisible() && ui.cbCol1->count() > 0;
 }
--- src/frontend/dockwidgets/HypothesisTestDock.h
+++ src/frontend/dockwidgets/HypothesisTestDock.h
@@ -1,14 +1,14 @@
-/***************************************************************************
+/*
 	File                 : HypothesisTestDock.h
 	Project              : LabPlot
 	Description          : Dock for Hypothesis Tests
 	--------------------------------------------------------------------
-	SPDX-FileCopyrightText: 2019  Devanshu Agarwal (agarwaldevanshu8@gmail.com)
+	SPDX-FileCopyrightText: 2019  Devanshu Agarwal <agarwaldevanshu8@gmail.com>
 	SPDX-FileCopyrightText: 2023-205 Alexander Semke <alexander.semke@web.de>
 	SPDX-FileCopyrightText: 2025 Kuntal Bar <barkuntal6@gmail.com>
 
- SPDX-License-Identifier: GPL-2.0-or-later
-***************************************************************************/
+	SPDX-License-Identifier: GPL-2.0-or-later
+*/
 
 #ifndef HYPOTHESISTESTDOCK_H
 #define HYPOTHESISTESTDOCK_H
