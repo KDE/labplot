a9db1f5eda7c58e061cdb102533344c2a9163564
Martin Marmsoler <martin.marmsoler@gmail.com>
2025-06-10 14:53:44 +0200

    cleanup

--- src/backend/datapicker/Datapicker.h
+++ src/backend/datapicker/Datapicker.h
@@ -61,7 +61,7 @@ public:
 	void save(QXmlStreamWriter*) const override;
 	bool load(XmlStreamReader*, bool preview) override;
 
-	void setColorPalette(const KConfig& config);
+	void setColorPalette(const KConfig&);
 	QColor themeColorPalette(int index) const;
 
 public Q_SLOTS:
