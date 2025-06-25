0090faa5aab7a135c548f69d172a7b47f98ee279
Alexander Semke <alexander.semke@web.de>
2025-06-25 22:04:48 +0200

    More fixes for problems caused during the conflict resolution.

---
+++ src/frontend/pivot/HierarchicalHeaderView.h
@@ -0,0 +1,136 @@
+/*
+	File                 : HierarchicalHeaderView.h
+	Project              : LabPlot
+	Description          : Hierarchical header view
+	--------------------------------------------------------------------
+	SPDX-FileCopyrightText: 2016 Lee Cho Kang <pzesseto@gmail.com>
+	SPDX-FileCopyrightText: 2023 Alexander Semke <alexander.semke@web.de>
+	SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
+#ifndef HIERARCHICALHEADERVIEW_H
+#define HIERARCHICALHEADERVIEW_H
+
+#include <QHeaderView>
+#include <QModelIndex>
+
+
+#include <QAbstractTableModel>
+#include <QHash>
+#include <QPair>
+
+enum eRbHeaderRole {
+	COLUMN_SPAN_ROLE = Qt::UserRole + 1,
+	ROW_SPAN_ROLE,
+	COLUMN_SIZE_ROLE,
+	ROW_SIZE_ROLE
+};
+
+class HierarchicalHeaderItem {
+public:
+	HierarchicalHeaderItem(HierarchicalHeaderItem* parent = nullptr);
+	HierarchicalHeaderItem(int row, int column, HierarchicalHeaderItem* parent = nullptr);
+	~HierarchicalHeaderItem();
+
+	HierarchicalHeaderItem* insertChild(int row, int col);
+	const HierarchicalHeaderItem* child(int row,int col) const;
+	HierarchicalHeaderItem* child(int row,int col);
+
+	void setData(const QVariant& data, int role);
+	QVariant data(int role=Qt::UserRole+1) const;
+
+	int column() const;
+	int row() const;
+	HierarchicalHeaderItem* parent();
+	void setText(const QString&);
+	void clear();
+
+private:
+	int row_prop;
+	int column_prop;
+	HierarchicalHeaderItem* parent_item;
+	QHash<QPair<int,int>,HierarchicalHeaderItem*> child_items;
+	QHash<int,QVariant> role_datas;
+};
+
+/****************************************************************************************************
+ *
+ *                              MODEL DECLARATIONS
+ * *************************************************************************************************/
+
+class HierarchicalHeaderModel: public QAbstractTableModel {
+	Q_OBJECT
+public:
+	HierarchicalHeaderModel(QObject* parent = nullptr);
+	HierarchicalHeaderModel(int rows, int cols, QObject* parent = nullptr);
+	virtual ~HierarchicalHeaderModel();
+
+public:
+	void setRowCount(int);
+	void setColumnCount(int);
+	void setSpan(int row, int column, int rowSpanCount, int columnSpanCount);
+	QModelIndex index(int row, int column, const QModelIndex& parent = QModelIndex()) const override;
+	int rowCount(const QModelIndex &parent=QModelIndex()) const override;
+	int columnCount(const QModelIndex &parent=QModelIndex()) const override;
+	QVariant data(const QModelIndex &index, int role) const override;
+	bool setData(const QModelIndex & index, const QVariant & value, int role = Qt::EditRole) override;
+	Qt::ItemFlags flags(const QModelIndex &index) const override;
+	void clear();
+
+    void setBaseSectionSize(QSize size);
+    void setOrientation(Qt::Orientation orient);
+private:
+	int m_rowCount{0};
+	int m_columnCount{0};
+	HierarchicalHeaderItem* m_rootItem{nullptr};
+
+    int *maxWidthArr;
+    QSize baseSectionSize;
+    Qt::Orientation orientation;
+};
+
+/****************************************************************************************************
+ *
+ *                              VIEW DECLARATIONS
+ * *************************************************************************************************/
+
+class HierarchicalHeaderView : public QHeaderView {
+	Q_OBJECT
+public:
+	HierarchicalHeaderView(Qt::Orientation orientation, QWidget* parent = nullptr);
+	HierarchicalHeaderView(Qt::Orientation orientation, int rows, int columns, QWidget* parent = nullptr);
+	~HierarchicalHeaderView() override;
+
+	HierarchicalHeaderModel* hierarchicalModel() const;
+	void setRowHeight(int row, int height);
+	void setColumnWidth(int col, int width);
+	void setCellBackgroundColor(const QModelIndex&, const QColor&);
+	void setCellForegroundColor(const QModelIndex&, const QColor&);
+
+    QSize getBaseSectionSize() const;
+    void setNewModel(HierarchicalHeaderModel* model);
+
+protected:
+	void mousePressEvent(QMouseEvent*) override;
+	QModelIndex indexAt(const QPoint&) const override;
+	void paintSection(QPainter* painter, const QRect& rect, int logicalIndex) const override;
+	QSize sectionSizeFromContents(int logicalIndex) const override;
+
+	QModelIndex columnSpanIndex(const QModelIndex&) const;
+	QModelIndex rowSpanIndex(const QModelIndex&) const;
+	int columnSpanSize(int row, int from, int spanCount) const;
+	int rowSpanSize(int column, int from, int spanCount) const;
+	int getSectionRange(QModelIndex& index, int* beginSection, int* endSection) const;
+
+protected Q_SLOTS:
+	void onSectionResized(int logicalIndex,int oldSize,int newSize);
+
+Q_SIGNALS:
+	void sectionPressed(int from, int to);
+
+private:
+	HierarchicalHeaderModel* m_model{nullptr};
+    QSize baseSectionSize;
+};
+
+#endif
---
+++ src/frontend/pivot/PivotTableView.cpp
@@ -0,0 +1,224 @@
+/***************************************************************************
+    File                 : PivotTableView.cpp
+    Project              : LabPlot
+    Description          : View class for PivotTable
+    --------------------------------------------------------------------
+    Copyright            : (C) 2019-2023 by Alexander Semke (alexander.semke@web.de)
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor,                    *
+ *   Boston, MA  02110-1301  USA                                           *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "PivotTableView.h"
+#include "HierarchicalHeaderView.h"
+#include "backend/pivot/PivotTable.h"
+#include "backend/lib/macros.h"
+#include "backend/lib/trace.h"
+
+#include <QInputDialog>
+#include <QFile>
+#include <QHBoxLayout>
+#include <QPainter>
+#include <QPrinter>
+#include <QPrintDialog>
+#include <QPrintPreviewDialog>
+#include <QTableView>
+
+#include <KConfigGroup>
+#include <KLocalizedString>
+#include <KMessageBox>
+#include <KSharedConfig>
+
+/*!
+	\class PivotTableView
+	\brief View class for PivotTable
+
+	\ingroup commonfrontend
+ */
+PivotTableView::PivotTableView(PivotTable* pivotTable, bool readOnly) : QWidget(),
+	m_pivotTable(pivotTable),
+	m_tableView(new QTableView(this)),
+	m_horizontalHeaderView(new HierarchicalHeaderView(Qt::Horizontal, m_tableView)),
+	m_verticalHeaderView(new HierarchicalHeaderView(Qt::Vertical, m_tableView)),
+	m_readOnly(readOnly) {
+
+	auto* layout = new QHBoxLayout(this);
+	layout->setContentsMargins(0,0,0,0);
+	layout->addWidget(m_tableView);
+
+	m_tableView->setSelectionMode(QAbstractItemView::ExtendedSelection);
+
+	if (m_readOnly)
+		m_tableView->setEditTriggers(QTableView::NoEditTriggers);
+
+	m_tableView->setHorizontalHeader(m_horizontalHeaderView);
+	m_horizontalHeaderView->setHighlightSections(true);
+	m_horizontalHeaderView->setSectionResizeMode(QHeaderView::ResizeToContents);
+	m_horizontalHeaderView->setSectionsClickable(true);
+
+	m_tableView->setVerticalHeader(m_verticalHeaderView);
+	m_verticalHeaderView->setHighlightSections(true);
+	m_verticalHeaderView->setSectionResizeMode(QHeaderView::ResizeToContents);
+	m_verticalHeaderView->setSectionsClickable(true);
+
+	init();
+}
+
+PivotTableView::~PivotTableView() = default;
+
+void PivotTableView::init() {
+	initActions();
+	initMenus();
+
+	//models
+	//TODO: at the moment we keep the data model in m_pivotTable, the header models are kept in HierarchicalHeaderView.
+	//re-design this. Let's keep all the models in m_pivotTable and set them here for the views.
+	m_tableView->setModel(m_pivotTable->dataModel());
+	m_pivotTable->setHorizontalHeaderModel(m_horizontalHeaderView->hierarchicalModel());
+	m_pivotTable->setVerticalHeaderModel(m_verticalHeaderView->hierarchicalModel());
+
+	connect(m_pivotTable, &PivotTable::changed, this, &PivotTableView::changed);
+}
+
+void PivotTableView::initActions() {
+
+}
+
+void PivotTableView::initMenus() {
+
+}
+
+void PivotTableView::connectActions() {
+
+}
+
+void PivotTableView::fillToolBar(QToolBar* toolBar) {
+	Q_UNUSED(toolBar);
+}
+
+/*!
+ * Populates the menu \c menu with the pivot table and pivot table view relevant actions.
+ * The menu is used
+ *   - as the context menu in PivotTableView
+ *   - as the "pivot table menu" in the main menu-bar (called form MainWin)
+ *   - as a part of the pivot table context menu in project explorer
+ */
+void PivotTableView::createContextMenu(QMenu* menu) {
+	Q_ASSERT(menu);
+}
+
+void PivotTableView::goToCell() {
+	bool ok;
+
+	int col = QInputDialog::getInt(nullptr, i18n("Go to Cell"), i18n("Enter column"), 1, 1, m_tableView->model()->columnCount(), 1, &ok);
+	if (!ok) return;
+
+	int row = QInputDialog::getInt(nullptr, i18n("Go to Cell"), i18n("Enter row"), 1, 1, m_tableView->model()->rowCount(), 1, &ok);
+	if (!ok) return;
+
+	goToCell(row-1, col-1);
+}
+
+void PivotTableView::goToCell(int row, int col) {
+	const auto& index = m_tableView->model()->index(row, col);
+	m_tableView->scrollTo(index);
+	m_tableView->setCurrentIndex(index);
+}
+
+bool PivotTableView::exportView() {
+	return true;
+}
+
+bool PivotTableView::printView() {
+	QPrinter printer;
+	auto* dlg = new QPrintDialog(&printer, this);
+	dlg->setWindowTitle(i18nc("@title:window", "Print Pivot Table"));
+
+	bool ret;
+	if ((ret = dlg->exec()) == QDialog::Accepted)
+		print(&printer);
+
+	delete dlg;
+	return ret;
+}
+
+bool PivotTableView::printPreview() {
+	QPrintPreviewDialog* dlg = new QPrintPreviewDialog(this);
+	connect(dlg, &QPrintPreviewDialog::paintRequested, this, &PivotTableView::print);
+	return dlg->exec();
+}
+
+/*!
+  prints the complete spreadsheet to \c printer.
+ */
+void PivotTableView::print(QPrinter* printer) const {
+	WAIT_CURSOR;
+	QPainter painter (printer);
+
+	RESET_CURSOR;
+}
+
+void PivotTableView::changed() {
+     qDebug() << "in PivotTableView::changed()";
+	// m_pivotTable->setHorizontalHeaderModel(m_horizontalHeaderView->hierarchicalModel());
+	// m_pivotTable->setVerticalHeaderModel(m_verticalHeaderView->hierarchicalModel());
+
+
+     HierarchicalHeaderModel* horizontalHeaderModel = static_cast<HierarchicalHeaderModel*>(m_horizontalHeaderView->hierarchicalModel());
+     HierarchicalHeaderModel* verticalHeaderModel = static_cast<HierarchicalHeaderModel*>(m_verticalHeaderView->hierarchicalModel());
+
+     horizontalHeaderModel->setOrientation(Qt::Horizontal);
+     verticalHeaderModel->setOrientation(Qt::Vertical);
+
+//     qDebug() << " setting size for horizontal header";
+//     qDebug() << " rows, cols = " << horizontalHeaderModel->rowCount() << ", " << horizontalHeaderModel->columnCount();
+     horizontalHeaderModel->setBaseSectionSize(m_horizontalHeaderView->getBaseSectionSize());
+
+//     qDebug() << "settign size for vertical header";
+//     qDebug() << " rows, cols = " << verticalHeaderModel->rowCount() << ", " << verticalHeaderModel->columnCount();
+     verticalHeaderModel->setBaseSectionSize(m_verticalHeaderView->getBaseSectionSize());
+}
+
+void PivotTableView::exportToFile(const QString& path, const bool exportHeader, const QString& separator, QLocale::Language language) const {
+	Q_UNUSED(exportHeader);
+	Q_UNUSED(separator);
+	Q_UNUSED(language);
+	QFile file(path);
+	if (!file.open(QFile::WriteOnly | QFile::Truncate))
+		return;
+
+	PERFTRACE(QLatin1String("export pivot table to file"));
+}
+
+void PivotTableView::exportToLaTeX(const QString & path, const bool exportHeaders,
+                                    const bool gridLines, const bool captions, const bool latexHeaders,
+                                    const bool skipEmptyRows, const bool exportEntire) const {
+	Q_UNUSED(exportHeaders);
+	Q_UNUSED(gridLines);
+	Q_UNUSED(captions);
+	Q_UNUSED(latexHeaders);
+	Q_UNUSED(skipEmptyRows);
+	Q_UNUSED(exportEntire);
+	QFile file(path);
+	if (!file.open(QFile::WriteOnly | QFile::Truncate))
+		return;
+
+	PERFTRACE(QLatin1String("export pivot table to latex"));
+}
