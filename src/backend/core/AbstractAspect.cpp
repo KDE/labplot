1e003b94f7d4d11cf7897c166c757d7d63aa7f03
Martin Marmsoler <murtel@hotmail.de>
2025-11-27 13:47:59 +0000

    Fix cantor link
    Reason: Notebook is not bundled in liblabplotbackend and therefore the definitions of the virtual functions are not available which are required

--- src/backend/core/AbstractAspect.cpp
+++ src/backend/core/AbstractAspect.cpp
@@ -1113,7 +1113,7 @@ void AbstractAspect::childSelected(const AbstractAspect* aspect) {
 	auto* parent = this->parentAspect();
 	if (parent && !parent->inherits<Folder>() && !parent->inherits<XYFitCurve>() && !parent->inherits<XYSmoothCurve>()
 #ifdef HAVE_CANTOR_LIBS
-		&& !parent->inherits<Notebook>()
+		&& !parent->inherits(AbstractAspect::typeName(AbstractType::Notebook).data())
 #endif
 	)
 		Q_EMIT this->selected(aspect);
@@ -1130,7 +1130,7 @@ void AbstractAspect::childDeselected(const AbstractAspect* aspect) {
 	auto* parent = this->parentAspect();
 	if (parent && !parent->inherits<Folder>() && !parent->inherits<XYFitCurve>() && !parent->inherits<XYSmoothCurve>()
 #ifdef HAVE_CANTOR_LIBS
-		&& !parent->inherits<Notebook>()
+		&& !parent->inherits(AbstractAspect::typeName(AbstractType::Notebook).data())
 #endif
 	)
 		Q_EMIT this->deselected(aspect);
