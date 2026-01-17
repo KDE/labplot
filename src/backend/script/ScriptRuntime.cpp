da7a5fc62c1411efc39ad112a5e65eab6c8ef745
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2026-01-17 19:50:06 +0100

    Python scripting: fix message output and format

--- src/backend/script/ScriptRuntime.cpp
+++ src/backend/script/ScriptRuntime.cpp
@@ -7,8 +7,8 @@
 	SPDX-License-Identifier: GPL-2.0-or-later
 */
 
-#include "Script.h"
 #include "ScriptRuntime.h"
+#include "Script.h"
 
 #include <KLocalizedString>
 /*!
--- src/backend/script/python/PythonLogger.cpp
+++ src/backend/script/python/PythonLogger.cpp
@@ -7,6 +7,8 @@
 	SPDX-License-Identifier: GPL-2.0-or-later
 */
 
+#include <backend/lib/macros.h>
+
 #include "PythonLogger.h"
 #include "PythonScriptRuntime.h"
 
